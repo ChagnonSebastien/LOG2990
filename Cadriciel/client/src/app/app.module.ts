3a5736fad18026eef2e5ac64b53440ef8ebaaa5b
Sebastien C <sebastien.chagnon@polymtl.ca>
2017-09-04 20:52:21 -0400

    Added a file forgotten in the last commit

--- Cadriciel/client/src/app/app.module.ts
+++ Cadriciel/client/src/app/app.module.ts
@@ -12,6 +12,13 @@ import { RacingGameComponent } from './racing-game/racing-game.component';
 import { CrosswordGameComponent } from './crossword-game/crossword-game.component';
 import {AppRoutingModule} from './app-routing.module';
 import {APP_BASE_HREF} from '@angular/common';
+import { InGameComponent } from './racing-game/in-game/in-game.component';
+import { RacingHeaderComponent } from './racing-game/racing-header/racing-header.component';
+import { TrackEditorComponent } from './racing-game/racing-header/track-editor/track-editor.component';
+import { AdministrationComponent } from './racing-game/racing-header/administration/administration.component';
+import { TracksComponent } from './racing-game/racing-header/tracks/tracks.component';
+import { TrackAdminComponent } from './racing-game/racing-header/tracks/track-admin/track-admin.component';
+import { TrackInfoComponent } from './racing-game/racing-header/tracks/track-info/track-info.component';
 
 
 @NgModule({
@@ -20,7 +27,14 @@ import {APP_BASE_HREF} from '@angular/common';
     CubeComponent,
     HomeComponent,
     RacingGameComponent,
-    CrosswordGameComponent
+    CrosswordGameComponent,
+    InGameComponent,
+    RacingHeaderComponent,
+    TrackEditorComponent,
+    AdministrationComponent,
+    TracksComponent,
+    TrackAdminComponent,
+    TrackInfoComponent
   ],
   imports: [
     BrowserModule,
