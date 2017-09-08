9eb73f90d6a2ea0c78cde2c8f3b1275b65780231
AudreyL <audrey.labrie@polymtl.ca>
2017-09-08 13:45:54 -0400

    game option mode and level to crossword game configuration interface

--- Cadriciel/client/src/app/crossword-game/crossword-game.component.ts
+++ Cadriciel/client/src/app/crossword-game/crossword-game.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crossword-game.component.css']
 })
 export class CrosswordGameComponent implements OnInit {
+    option: string = null;
+    mode: string = null;
+    level: string = null;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setOption(chosenOption: string): void {
+      this.option = chosenOption;
+  }
+
+  setMode(chosenMode: string): void {
+      this.option = chosenMode;
+  }
+
+  setLevel(chosenLevel: string): void {
+      this.level = chosenLevel;
   }
 
 }
