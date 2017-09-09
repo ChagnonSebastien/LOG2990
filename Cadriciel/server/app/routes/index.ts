ff34e45a336352f1b18f86912a4a1e4d69ba1004
AudreyL <audrey.labrie@polymtl.ca>
2017-09-08 22:22:10 -0400

    server able to read lexicon and store in list

--- Cadriciel/server/app/routes/index.ts
+++ Cadriciel/server/app/routes/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import { Message } from '../../../commun/communication/message';
 import { LexiconApi } from './lexicon-api';
 import { Request } from 'express';
+import { FileReader } from '../file-reader';
 
 module Route {
     let lexicon = new LexiconApi;
@@ -20,7 +21,21 @@ module Route {
             let word = req.params.word;
             message.title = 'Hello';
             message.body = word;
-            res.send(JSON.stringify(message));
+            res.send(JSON.stringify({"title":"hello"}));
+
+        }
+
+        public englishWordLexicon(red: express.Request, res: express.Response, next: express.NextFunction) {
+            let fileReader = new FileReader();
+            let lexicon: string[] = fileReader.readfile(fileReader.filepath);
+            //let wordList = lexicon.split("\n")
+            //res.sendFile('../../server/englishWords.txt');
+
+            //res.send("Hello");
+            //res.send(JSON.stringify({"title":"hello"}));
+            //console.log(lexicon.split('\n'));
+
+            res.send(JSON.stringify({"title": "see console for lexicon"}));
         }
     }
 }
