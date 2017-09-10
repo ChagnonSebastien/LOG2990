426604c5aa011bf0e02683e41fcb6c015ddf5921
Eric Li <eric-li@hotmail.ca>
2017-09-09 23:57:27 -0400

    wordDefinition works, see 'localhost:3000/api/definition/:word', implemented in index.ts

--- Cadriciel/server/app/routes/index.ts
+++ Cadriciel/server/app/routes/index.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import { Message } from '../../../commun/communication/message';
 import { LexiconApi } from './lexicon-api';
-import { Request } from 'express';
+import * as request from 'request';
 import { LexiconReader } from '../lexicon-reader';
 
 module Route {
@@ -17,13 +17,13 @@ module Route {
         }
 
         public wordDefinition(req: express.Request, res: express.Response, next: express.NextFunction) {
-            const message = new Message();
-            
+            let uri = 'http://api.wordnik.com:80/v4/word.json';
+            let options = 'definitions?limit=1&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
             let word = req.params.word;
-            message.title = 'Hello';
-            message.body = word;
-            res.send(JSON.stringify(message));
-
+            request(`${uri}/${word}/${options}`, (error, response, body) => {
+                body = JSON.parse(body);
+                res.send(JSON.stringify(body[0].text));
+            })
         }
 
         public englishWordLexicon(red: express.Request, res: express.Response, next: express.NextFunction) {
