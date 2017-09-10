5aab6493e545d60852ae9d1f69a8dc91db98b7d7
Eric Li <eric-li@hotmail.ca>
2017-09-10 16:57:40 -0400

    Cleaned up files, formatted admin-routing.module (4 space indent), and deleted crossword folder

--- Cadriciel/client/src/app/admin/admin-routing.module.ts
+++ Cadriciel/client/src/app/admin/admin-routing.module.ts
@@ -1,12 +1,12 @@
-import { NgModule }            from '@angular/core';
-import { RouterModule }        from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AdminComponent }    from './admin.component';
+import { AdminComponent } from './admin.component';
 
 @NgModule({
-  imports: [RouterModule.forChild([
-    { path: 'admin', component: AdminComponent}
-  ])],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild([
+        { path: 'admin', component: AdminComponent }
+    ])],
+    exports: [RouterModule]
 })
-export class AdminRoutingModule {}
+export class AdminRoutingModule { }
