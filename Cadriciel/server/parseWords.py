c7f63989ebe6cece14ba27d90c263f9d757f01e7
Eric Li <eric-li@hotmail.ca>
2017-09-10 10:21:59 -0400

    Used Python to generate englishWords-sorted.json (lexicon sorted by length first, then alphabetical order)

---
+++ Cadriciel/server/parseWords.py
@@ -0,0 +1,11 @@
+# Extract list of words
+with open("englishWords.txt") as f:
+    words = map(str.strip, f.readlines())
+
+# Sort list of words by length ascending
+sortedWords = sorted(words, key=lambda x: len(x))
+
+# Dump sorted words as JSON
+import json
+with open('englishWords-sorted.json', 'w') as outfile:
+    json.dump(sortedWords, outfile)
