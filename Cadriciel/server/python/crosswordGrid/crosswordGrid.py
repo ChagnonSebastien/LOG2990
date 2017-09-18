b8f3067912c633e8114209199ad99d712dd11f8e
Eric Li <eric-li@hotmail.ca>
2017-09-17 21:31:56 -0400

    Completed generateGrid that returns an empty crossword grid every 0.5 seconds with at least 24 three-letter words (every 0.05 seconds at least 20 three-letter words)

--- Cadriciel/server/python/crosswordGrid/crosswordGrid.py
+++ Cadriciel/server/python/crosswordGrid/crosswordGrid.py
@@ -1,8 +1,16 @@
+from random import shuffle
+
 class CrosswordGrid:
-    "Crossword Grid Generator"
+    'Crossword Grid Generator'
     def __init__(self, gridSize):
         self.gridSize = gridSize
-        self.grid = [[" " for x in range(gridSize)] for y in range(gridSize)]
+        self.availableSquares = [(i, j) for i in range(self.gridSize) for j in range(self.gridSize)]                        
+        self.resetGrid()
+
+    def resetGrid(self):
+        self.lines = []        
+        self.grid = [ [' ' for j in range(self.gridSize)] for i in range(self.gridSize) ]
+        shuffle(self.availableSquares)
 
     def addLetter(self, letter, line, column):
         self.grid[line][column] = letter
@@ -10,45 +18,75 @@ class CrosswordGrid:
     def addBlackSquare(self, line, column):
         self.addLetter('#', line, column)
 
+    def erase(self, line, column):
+        self.grid[line][column] = ' '
+
     def printGrid(self):
         for line in self.grid:
-            print "|",
+            print '|',
             for letter in line:
-                print letter + " |",
-            print "\n-" + "-" * 4 * self.gridSize
+                print letter + ' |',
+            print '\n-' + '-' * 4 * self.gridSize
 
     def isValidHorizontal(self, grid):
         for line in grid:
-            sections = [section for section in ''.join(line).split('#') if section != '']
-            if len(sections) > 2:
+            sections = [ len(section) for section in ''.join(line).split('#') if section != '' ]
+            if len(sections) > 2 or min(sections) < 3:
                 return False
+            self.lines += sections
         return True
 
     def transposeGrid(self, grid):
-        return [[grid[i][j] for i in range(self.gridSize)] for j in range(self.gridSize)]
+        return [ [grid[i][j] for i in range(self.gridSize)] for j in range(self.gridSize) ]
 
     def isValidGrid(self):
+        self.lines = []
         return self.isValidHorizontal(self.grid) and self.isValidHorizontal(self.transposeGrid(self.grid))
+
+    def score(self):
+        return self.lines.count(3)
+
+    def generateGrid(self):
+        score = 0
+        while score < 24:
+            self.resetGrid()
+            #print 'Score is', score
+            for coordinate in self.availableSquares:
+                self.addBlackSquare(coordinate[0], coordinate[1])
+                if not self.isValidGrid():
+                    self.erase(coordinate[0], coordinate[1])
+            score = self.score()
 
     def testAll(self):
         # Test cases for isValidGrid
         assert(self.isValidGrid())
         self.addBlackSquare(0,0)
         assert(self.isValidGrid())
-        self.addBlackSquare(0,6)
+        self.addBlackSquare(0,5)
         assert(self.isValidGrid())
         self.addBlackSquare(0,9)
         assert(self.isValidGrid())
-        self.addBlackSquare(8,0)
+        self.addBlackSquare(6,0)
         assert(self.isValidGrid())
-        self.addBlackSquare(5,0)
+        self.addBlackSquare(1,0)
+        assert(self.isValidGrid())
+        self.addBlackSquare(9,0)
         assert(not self.isValidGrid())
-        # Reset grid
-        self.grid = [[" " for x in range(self.gridSize)] for y in range(self.gridSize)]
+        
+        self.resetGrid()
         print 'CrosswordGrid class: all tests passed'
 
 # Automatic unit tests
 prototypeGrid = CrosswordGrid(10)
 prototypeGrid.testAll()
 
-# 
+# Generate random crossword
+import timeit
+nTests = 100
+start = timeit.default_timer()
+for i in range(nTests):
+    prototypeGrid.generateGrid()
+    prototypeGrid.printGrid()
+    print ''
+end = timeit.default_timer()
+print 'Average time:', (end - start) / nTests
