a05a7b31d77d08a9a06d7976f0bea5ce037917cd
Eric Li <eric-li@hotmail.ca>
2017-09-18 08:00:36 -0400

    Added method that's able to find all words that match a pattern

--- Cadriciel/server/python/crosswordGrid/crossword.py
+++ Cadriciel/server/python/crosswordGrid/crossword.py
@@ -1,6 +1,8 @@
 from crosswordGrid import *
 import json
 import pprint
+import re
+from string import maketrans
 
 class Crossword:
     'Crossword prototype'
@@ -16,6 +18,16 @@ class Crossword:
     def getWordsOfLength(self, length):
         return self.lexicon["common"][str(length)] + self.lexicon["uncommon"][str(length)]
 
+    def patternToRegex(self, pattern):
+        return re.sub(r'\s','[a-z]', pattern)
+
+    def allWordsThatMatchPattern(self, pattern):
+        wordsOfSameLength = self.getWordsOfLength(len(pattern))
+        regex = re.compile(self.patternToRegex(pattern))
+        return filter(regex.search, wordsOfSameLength)
+
 gridGenerator = CrosswordGrid(10)
 crossword = Crossword(gridGenerator, 'lexicon.json')
 print len(crossword.getWordsOfLength(9))
+
+print crossword.allWordsThatMatchPattern('h   n')
