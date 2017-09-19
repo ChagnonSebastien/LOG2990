c58a0a7925ffacb97c0aae24de749cf410971c5f
AudreyL <audrey.labrie@polymtl.ca>
2017-09-19 07:55:13 -0400

    Tested lexiconreader.readwords method. All tests passed.

--- Cadriciel/server/app/lexicon-reader.spec.ts
+++ Cadriciel/server/app/lexicon-reader.spec.ts
@@ -3,10 +3,33 @@ import { LexiconReader } from "./lexicon-reader";
 
 describe('LexiconReader', ()=>{
     let lexiconFilePath: string = '../server/lexicon/englishWords.txt';
+    let lexiconReader: LexiconReader = new LexiconReader();
 
-    it("readWords method", ()=>{
-        //let lexiconReader = new LexiconReader();
-        //let firstWord:string = lexiconReader.readWords(lexiconFilePath)[0];
-        assert(true);
+    it("Should return the first word of the lexicon ", ()=>{
+        let firstWord:string = lexiconReader.readWords(lexiconFilePath)[0];
+
+        assert(firstWord == 'aalii');
     });
+
+    it("Should return the last word of the lexicon ", ()=>{
+        let words: string[] = lexiconReader.readWords(lexiconFilePath);
+        let lastWord: string = words[words.length - 1];
+
+        assert(lastWord == 'zymotic');
+    });
+
+    it("Should return the word in the middle of the lexicon ", ()=>{
+        let words: string[] = lexiconReader.readWords(lexiconFilePath);
+        let middleWord: string = words[words.length/2];
+
+        assert(middleWord == 'jocosity');
+    });
+
+    it("Should not return an empty array as the lexicon ", ()=>{
+        let words: string[] = lexiconReader.readWords(lexiconFilePath);
+
+        assert(words != []);
+    });
+
+    
 });
--- Cadriciel/server/app/routes/lexicon.spec.ts
+++ Cadriciel/server/app/routes/lexicon.spec.ts
@@ -2,13 +2,5 @@ import { Lexicon } from './lexicon';
 import {assert} from 'chai';
 
 describe('Lexicon', ()=>{
-    let lexiconFilePath: string = '../server/lexicon/englishWords.txt';
-    //let lexicon: Lexicon = new Lexicon();
-
-    it("englishWordLexicon method", ()=>{
-        //let lexiconReader = new LexiconReader();
-        //let firstWord:string = lexiconReader.readWords(lexiconFilePath)[0];
-
-        assert(true);
-    });
+    
 });
