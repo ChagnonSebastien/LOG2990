8ca7617778c79bbeadf50f6fb2b43cb922d9cb3d
Eric Li <eric-li@hotmail.ca>
2017-09-20 21:35:04 -0400

    Tests done for crossword-game.component.ts

--- Cadriciel/client/src/app/crossword-game/crossword-game.component.spec.ts
+++ Cadriciel/client/src/app/crossword-game/crossword-game.component.spec.ts
@@ -22,4 +22,24 @@ describe('CrosswordGameComponent', () => {
     it('should be created', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should set option to SOLO', () => {
+        component.setOption('SOLO');
+        expect(component.option).toEqual('SOLO');
+    });
+
+    it('should set mode to CLASSIC', () => {
+        component.setMode('CLASSIC');
+        expect(component.mode).toEqual('CLASSIC');
+    });
+
+    it('should set level to EASY', () => {
+        component.setLevel('EASY');
+        expect(component.level).toEqual('EASY');
+    });
+
+    it('should return True when option is SOLO', () => {
+        component.setOption('SOLO');
+        expect(component.isSolo()).toBeTruthy();
+    });
 });
