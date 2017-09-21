eca946ac14049f91dc43c6e31dd6eb86b66bf7a4
Eric Li <eric-li@hotmail.ca>
2017-09-21 17:22:48 -0400

    getTracks api endpoint works (all tests passed)

--- Cadriciel/server/app/routes/track.spec.ts
+++ Cadriciel/server/app/routes/track.spec.ts
@@ -4,10 +4,38 @@ describe('Track', () => {
     const chai = require('chai');
     const chaiHttp = require('chai-http');
     chai.use(chaiHttp);
-
+    const apiUrl = 'http://localhost:3000/api';
 
     it('Should be true', (done) => {
         assert(true);
         done();
+    });
+
+    it('Should create a new track in the database', (done) => {
+        chai.request(apiUrl)
+            .post('/tracks')
+            .send({
+                id: "1",
+                name: "test",
+                description: "this is a test",
+                type: "pro",
+                trackIntersections: [{ "x": 1, "y": 1 }],
+                puddles: [{ "distance": 1, "offset": 1 }],
+                potholes: [{ "distance": 1, "offset": 1 }],
+                boosters: [{ "distance": 1, "offset": 1 }]
+            }).end((err: any, res: any) => {
+                assert(JSON.parse(res.text).data === 'success');
+                done();
+            });
+    });
+
+    it('Should return a list of tracks', (done) => {
+        chai.request(apiUrl)
+            .get('/tracks')
+            .end((err: any, res: any) => {
+                let tracks = JSON.parse(res.text);
+                assert(tracks.length > 0);
+                done();
+            });
     });
 });
