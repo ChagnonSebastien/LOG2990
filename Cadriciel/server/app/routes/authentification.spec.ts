a5c7dee39a0b8231f3c1a16a62c480c427004cd1
Eric Li <eric-li@hotmail.ca>
2017-09-20 21:05:07 -0400

    Tests completed for authentication.ts in server

--- Cadriciel/server/app/routes/authentification.spec.ts
+++ Cadriciel/server/app/routes/authentification.spec.ts
@@ -2,17 +2,48 @@ import { assert } from 'chai';
 import { Authentication } from './authentication';
 
 describe('Authentication', () => {
+    const apiUrl = 'http://localhost:3000/api';
     let chai = require('chai');
     let chaiHttp = require('chai-http');
     chai.use(chaiHttp);
 
 
-    it("testing login", (done) => {
-        chai.request('http://localhost:3000')
+    it('Should login when password is walleandtomato', (done) => {
+        chai.request(apiUrl)
             .post('/login')
-            .send({password: "walleandtomato"})
+            .send({password: 'walleandtomato'})
             .end((err: any, res: any) => {
-                assert(true);
+                assert(JSON.parse(res.text).data == 'authenticated');
+                done();
+            });
+    });
+
+    it('Should not login when password is not walleandtomato', (done) => {
+        chai.request(apiUrl)
+            .post('/login')
+            .send({password: 'blablabla'})
+            .end((err: any, res: any) => {
+                assert(JSON.parse(res.text).data != 'authenticated');
+                done();
+            });
+    });
+
+    it('Should change password when walleandtomato is entered', (done) => {
+        chai.request(apiUrl)
+            .post('/changepassword')
+            .send({oldPassword: 'walleandtomato', newPassword: 'walleandtomato'})
+            .end((err: any, res: any) => {
+                assert(JSON.parse(res.text).data == 'success');
+                done();
+            });
+    });
+
+    it('Should not change password when a password other than walleandtomato is entered', (done) => {
+        chai.request(apiUrl)
+            .post('/changepassword')
+            .send({oldPassword: 'blablabla', newPassword: 'walleandtomato'})
+            .end((err: any, res: any) => {
+                assert(JSON.parse(res.text).data == 'invalid');
                 done();
             });
     });
