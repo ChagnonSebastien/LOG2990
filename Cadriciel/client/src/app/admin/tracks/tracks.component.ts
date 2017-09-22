05914819f93945cac7426af51270499c0723320c
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2017-09-21 23:26:13 -0400

    tslint checkup

--- Cadriciel/client/src/app/admin/tracks/tracks.component.ts
+++ Cadriciel/client/src/app/admin/tracks/tracks.component.ts
@@ -2,23 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { Track } from './track';
 
 @Component({
-    selector: 'tracks',
+    selector: 'app-tracks',
     templateUrl: './tracks.component.html',
     styleUrls: ['./tracks.component.css'],
     providers: []
 })
 export class TracksComponent implements OnInit {
 
-    tracks: Track[];
-    selectedTrack: Track;
+    public tracks: Track[];
+    public selectedTrack: Track;
 
     constructor() {
 
     }
 
-    ngOnInit() {
-        
+    public ngOnInit() {
+
     }
-
-
 }
