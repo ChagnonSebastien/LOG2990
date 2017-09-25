8706b96f57643daba4e121dc682e34802477363f
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2017-09-24 23:50:07 -0400

    filled the method twoLineIntersection

--- Cadriciel/client/src/app/racing-game/draw-track/track-validation.service.ts
+++ Cadriciel/client/src/app/racing-game/draw-track/track-validation.service.ts
@@ -20,7 +20,7 @@ export class TrackValidationService {
 
     public addPoint(intersection: THREE.Vector3) {
         this.trackElements.push(
-            { intersection: intersection, intersectionAngle: false, segmentLengthValid: false, segmentIntersections: [] }
+            { intersection, intersectionAngle: false, segmentLengthValid: false, segmentIntersections: [] }
         );
         this.checkSegmentLength(this.trackElements.length - 2);
         this.checkSegmentIntersections(this.trackElements.length - 2);
@@ -48,6 +48,22 @@ export class TrackValidationService {
 
     public checkSegmentIntersections(index: number) {
 
+    }
+
+    public twoLineIntersection(
+        line1: {point1: {x: number, y: number}, point2: {x: number, y: number}},
+        line2: {point1: {x: number, y: number}, point2: {x: number, y: number}}
+    ): {x: number, y: number} {
+        const a1 = (line1.point2.y - line1.point1.y) / (line1.point2.x - line1.point1.x);
+        const b1 = (line1.point1.y - (a1 * line1.point1.x));
+
+        const a2 = (line2.point2.y - line2.point1.y) / (line2.point2.x - line2.point1.x);
+        const b2 = (line2.point1.y - (a1 * line2.point1.x));
+
+        const x = (b2 - b1) / (a1 - a2);
+        const y = (a1 * x) + b1;
+
+        return {x, y};
     }
 
     public checkPointAngle(index: number) {
