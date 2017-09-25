f858272c375cc74d614d36eb0c54bc00f398c120
J-P <jeanpaulcech@yahoo.ca>
2017-09-25 14:41:53 -0400

    version that compiles

--- Cadriciel/client/src/app/crossword-game/crossword-game.module.ts
+++ Cadriciel/client/src/app/crossword-game/crossword-game.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
-import { CrosswordGameComponent } from './crossword-game.component';
 import {CrosswordGameInterfaceComponent} from './crossword-game-interface.component';
+import { CrosswordGameComponent } from './crossword-game.component';
 import { CrosswordGameRoutingModule} from './crossword-game-routing.module';
 
 @NgModule({
@@ -12,8 +11,8 @@ import { CrosswordGameRoutingModule} from './crossword-game-routing.module';
         FormsModule,
         CrosswordGameRoutingModule
     ],
-    declarations: [CrosswordGameComponent, CrosswordGameInterfaceComponent],
-    exports: [CrosswordGameComponent, CrosswordGameInterfaceComponent],
+    declarations: [CrosswordGameComponent,CrosswordGameInterfaceComponent],
+    exports: [CrosswordGameComponent,CrosswordGameInterfaceComponent],
     providers: []
 })
 export class CrosswordGameModule { }
