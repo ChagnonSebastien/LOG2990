e67e5301605a41f46e7fcecfdb59a189511b8bf5
Michée Kamte Kouetche <michee.kamte-kouetche@polymtl.ca>
2017-09-26 23:13:13 -0400

    test camera view

--- Cadriciel/client/src/app/racing-game/camera.service.spec.ts
+++ Cadriciel/client/src/app/racing-game/camera.service.spec.ts
@@ -1,1 +1,22 @@
+import { CameraService } from './camera.service';
+import { TestBed, inject } from '@angular/core/testing';
+import * as THREE from 'three';
 
+describe('test drawTrackService', function() {
+    let cameraService: CameraService;
+    beforeEach(() => {
+        cameraService = new CameraService();
+    });
+
+    it('construction test', done => {
+        expect(cameraService).toBeTruthy();
+        done();
+    });
+
+    it('calculate ratio of window', done => {
+        const number1 = 10;
+        const number2 = 10;
+        expect(cameraService.getAspectRatio(number1, number2)).toEqual(1);
+        done();
+    });
+});
