2593f019167d23a5f76ac9b0d14657109d3a0093
Eric Li <eric-li@hotmail.ca>
2017-09-27 18:00:44 -0400

    TDD crossword.addLetter outOfRange edge case covered [4/4 tests]

--- Cadriciel/server/app/crossword.spec.ts
+++ Cadriciel/server/app/crossword.spec.ts
@@ -3,17 +3,34 @@ import { Crossword } from './crossword';
 
 describe('Crossword', () => {
     let crossword: Crossword;
+    const size = 10;
+
     beforeEach(() => {
-        crossword = new Crossword(10);
+        crossword = new Crossword(size);
     });
 
-    it('should be size 10 x 10', () => {
-        expect(crossword.grid.length).to.equal(10);
-        expect(crossword.grid[0].length).to.equal(10);
+    describe('getEmptyGrid(size: number) : String[][] { }', () => {
+        it('should return a grid with size 10 x 10', () => {
+            expect(crossword.grid.length).to.equal(10);
+            expect(crossword.grid[0].length).to.equal(10);
+        });
+
+        it('should return a grid with empty characters', () => {
+            const i = Math.floor(Math.random() * size) + 0;
+            const j = Math.floor(Math.random() * size) + 0;
+            expect(crossword.grid[i][j]).to.equal(' ');
+        });
     });
 
-    it('should insert the letter "a" at coordinates (3, 0)', () => {
-        crossword.addLetter(3, 0, 'a');
-        expect(crossword.grid[3][0]).to.equal('a');
+    describe('addLetter(i: number, j: number, letter: string) { }', () => {
+        it('should insert the letter "a" at coordinates (3, 0)', () => {
+            expect(crossword.addLetter(3, 0, 'a')).to.be.true;
+            expect(crossword.grid[3][0]).to.equal('a');
+        });
+
+        it('should not insert anything if the indexes are out of range', () => {
+            expect(crossword.addLetter(10, 0, 'a')).to.be.false;
+            expect(crossword.addLetter(0, 10, 'a')).to.be.false;
+        });
     });
 });
--- Cadriciel/server/app/crossword.ts
+++ Cadriciel/server/app/crossword.ts
@@ -1,15 +1,23 @@
 export class Crossword {
-    public grid: String[];
+    public size: number;
+    public grid: String[][];
 
     constructor(size: number) {
+        this.size = size;
         this.grid = this.getEmptyGrid(size);
     }
 
-    public getEmptyGrid(size: number): String[] {
+    public getEmptyGrid(size: number): String[][] {
         const line = Array(size).fill(' ');
         return Array(size).fill(line);
     }
 
-    public addLetter(i: number, j: number, letter: string) {
+    public addLetter(i: number, j: number, letter: string): boolean {
+        if (i >= this.size || j >= this.size) {
+            return false;
+        }
+
+        this.grid[i][j] = letter;
+        return true;
     }
 }
