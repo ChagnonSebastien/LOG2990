bb337480eff22dd98d4022ed730821f67485a037
jeanpaulcech <jeanpaulcech@yahoo.ca>
2017-09-26 21:25:55 -0400

    added functions to find the indexes of all letters of a word of a certain index

--- Cadriciel/client/src/app/crossword-game/crossword-game-interface.component.ts
+++ Cadriciel/client/src/app/crossword-game/crossword-game-interface.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./crossword-game-interface.component.css']
 })
 export class CrosswordGameInterfaceComponent implements OnInit {
-    public rawCrossword: [[String]] = [['0', '0', '0', '0', 'p', '0', '0', '0', '0', '0'],
+    public rawCrossword: [[string]] = [['0', '0', '0', '0', 'p', '0', '0', '0', '0', '0'],
                                       ['0', '0', '0', '0', 'u', '0', '0', '0', '0', '0'],
                                       ['0', '0', '0', '0', 'n', '0', 'b', '0', '0', '0'],
                                       ['0', 'h', 'y', 'd', 'e', 'r', 'a', 'b', 'a', 'd'],
@@ -22,53 +22,100 @@ export class CrosswordGameInterfaceComponent implements OnInit {
     // public indices: [String] = ['Education Hub', 'Information Technology Hub', 'Cultural Hub', 'Capital of India',
     // 'India financial capital', 'Saffron region'];
 
-    public listOfWordsAndHints: [{word: String, indice: String}] = [{word: 'pune', indice : 'Education Hub' },
-    {word: 'banglore', indice : 'Information Technology Hub'}, {word: 'hyderabad', indice : 'Cultural Hub'},
+    public listOfWordsAndHints: [{word: string, indice: string}] = [{word: 'pune', indice : 'Education Hub' },
+    {word: 'bangalore', indice : 'Information Technology Hub'}, {word: 'hyderabad', indice : 'Cultural Hub'},
     {word: 'delhi', indice : 'Capital of India'}, {word: 'mumbai', indice : 'India financial capital'},
     {word: 'kashmir', indice : 'Saffron region'} ];
-    public wordsIndexes: [[String, {i: number, j: number}]];
+
+    public wordsIndexes: {word: string, indexes: Index[], position: string}[] = [];
+
     public gridSqaures: [{i: number, j: number, isActive: boolean}];
     public activeSquareCoord: {i: number, j: number};
+
     public handleClick(i, j) {
           this.activeSquareCoord = {
             i: i,
             j: j
         };
-        var indexes :{i:number,j:number}[]= this.findWordIndexes('kashmir',this.rawCrossword[9],9,true);
-        console.log(indexes);
+
     }
     public isActive(i, j): boolean {
-       
+
         return (i === this.activeSquareCoord.i && j === this.activeSquareCoord.j);
      }
 
-     public(fillWordsIndexes){
-        
+     public fillWordsIndexes() {
+
+        for (let k = 0 ; k < this.listOfWordsAndHints.length ; k++ ) {
+
+            // check for rows first
+
+            for (let l = 0 ; l < this.rawCrossword.length ; l++ ) {
+
+                // get word indexes for a given row. Will return empty if not present in that row
+
+                let indexes: Index[] = this.findWordIndexes
+                (this.listOfWordsAndHints[k].word, this.rawCrossword[l], l, true);
+
+                if (indexes.length !== 0) {
+
+
+                    this.wordsIndexes.push({word: this.listOfWordsAndHints[k].word, indexes: indexes, position: 'horizontal'});
+                    break;
+
+            } else {
+
+                // try in column of given index
+
+                indexes = this.findWordIndexes
+                (this.listOfWordsAndHints[k].word, this.getColumn(l), l, false);
+
+                if (indexes.length !== 0) {
+                    this.wordsIndexes.push({word: this.listOfWordsAndHints[k].word, indexes: indexes, position: 'vertical'});
+                    break;
+              }
+
+            }
+         }
+
+       }
+    }
+
+     // Returns the indexes of a given word if it is contained in a grid column or row
+     public findWordIndexes(word: string, gridPart: string[], columnOrRowIndex: number, row: boolean) {
+
+        let indexes : Index[] = [];
+
+        for (let k = 0 ; k < gridPart.length && indexes.length !== word.length ; k++ ) {
+            if (gridPart[k] === word[indexes.length]) {
+
+                if (row) {
+                    indexes.push({i: columnOrRowIndex, j: k});
+                } else {
+                     indexes.push({i: k, j: columnOrRowIndex});
+                }
+
+            } else {
+                indexes.splice(0, indexes.length);
+            }
+        }
+            /*test for two special condions firt before returning
+            1. the first letter of the word is the last letter of the given column or row
+            2. make sure the word we found is not actually part of a larger word eg : cat could be found in catastrophic*/
+            if (indexes.length < word.length) {
+            indexes.splice(0, indexes.length);
+            }
+
+         return(indexes);
      }
 
-     public findWordIndexes(word:string,gridPart:[String], columnOrRowIndex:number,row:boolean){
-        
-        var indexes :{i:number,j:number}[]=[];
-        //indexes.push({i:1,j:1});
-        
-        for(var k = 0 ; k < gridPart.length && indexes.length !=word.length ;k++ )
-         {  //console.log(gridPart[k]);
-            if(gridPart[k]==word[k])
-            {   
-                if(row)
-                indexes.push({i:columnOrRowIndex,j:k})
-                   //indexes[k]=({i:columnOrRowIndex,j:k});
-                else 
-                indexes.push({i:k,j:columnOrRowIndex});
-            }
-            else 
-                {
-                
-                //console.log(indexes);
-                indexes.splice(0,indexes.length);}
-         }
-        // console.log(indexes);
-         return(indexes);
+     public getColumn(index: number) {
+        let column : string[] = [];
+
+        for ( let i = 0; i < 11; i++) {
+            column.push(this.rawCrossword[i][index]);
+        }
+        return column;
      }
 
     constructor() { }
@@ -81,3 +128,9 @@ export class CrosswordGameInterfaceComponent implements OnInit {
     }
 
 }
+
+interface Index {
+    i: number;
+    j: number;
+}
+
