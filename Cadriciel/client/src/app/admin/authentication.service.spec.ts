94f888bf61243226a12a17d94e9934b49910c4bd
Julio Dandjinou <julio-herve.dandjinou@polymtl.ca>
2017-09-30 07:33:30 -0400

    remove comments

--- Cadriciel/client/src/app/admin/authentication.service.spec.ts
+++ Cadriciel/client/src/app/admin/authentication.service.spec.ts
@@ -109,38 +109,5 @@ describe('AuthenticationService', () => {
         expect(result).toBeUndefined();
         expect(catchedError).toBeDefined();
       }));
-/*
-    it('should be created', inject([AuthenticationService], (service: AuthenticationService) => {
-        expect(service).toBeTruthy();
-    }));
 
-    it('should authenticate when the password is walleandtomato', inject([AuthenticationService], (service: AuthenticationService) => {
-        service.authenticate('walleandtomato').then(res => {
-            expect(res).toMatch('authenticated');
-        });
-    }));
-
-    it('should be invalid when the password is not walleandtomato', inject([AuthenticationService], (service: AuthenticationService) => {
-        service.authenticate('blablabla').then(res => {
-            expect(res).toMatch('invalid');
-        });
-    }));
-
-    it('should change passwords from walleandtomato to tomatoandwalle',
-        inject([AuthenticationService],
-        (service: AuthenticationService) => {
-            service.changePassword('walleandtomato', 'tomatoandwalle').then(res => {
-                expect(res).toMatch('success');
-                service.changePassword('tomatoandwalle', 'walleandtomato');
-            });
-    }));
-
-    it('should not change passwords when old password is invalid',
-    inject([AuthenticationService],
-    (service: AuthenticationService) => {
-            service.changePassword('blablabla', 'tomatoandwalle').then(res => {
-                expect(res).toMatch('invalid');
-            });
-    }));
-    */
 });
