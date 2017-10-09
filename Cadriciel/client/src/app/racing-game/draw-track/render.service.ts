4219815748db6ca60d1a2603454c014025a82536
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2017-10-09 10:58:59 -0400

    Moved the createScene method to the render service

--- Cadriciel/client/src/app/racing-game/draw-track/render.service.ts
+++ Cadriciel/client/src/app/racing-game/draw-track/render.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@angular/core';
+import * as THREE from 'three';
+
+const viewDepth = 10;
 
 @Injectable()
 export class RenderService {
 
     public container: HTMLElement;
 
+    private camera: THREE.OrthographicCamera;
+
+    private scene: THREE.Scene;
+
     public initialise(container: HTMLElement) {
         this.container = container;
+    }
+
+    private createScene() {
+        this.scene = new THREE.Scene();
+
+        this.camera = new THREE.OrthographicCamera(
+            this.container.clientWidth / - 2,
+            this.container.clientWidth / 2,
+            this.container.clientHeight / 2,
+            this.container.clientHeight / - 2,
+            -viewDepth,
+            viewDepth
+        );
     }
 }
