71ecf10821351b09dcb4a35f93266ae0152f3ee0
Eric <eric-li@hotmail.ca>
2017-10-11 11:48:52 -0400

    [feature | crossword-checker] added getWords()

--- Cadriciel/server/app/crossword-checker.spec.ts
+++ Cadriciel/server/app/crossword-checker.spec.ts
@@ -8,9 +8,26 @@ describe('CrosswordChecker', () => {
         crosswordGenerator.reset();
     });
 
-    describe('check()', () => {
+    describe('verify()', () => {
         it('should check if all the lines are valid', () => {
-            expect(CrosswordChecker.verify(crosswordGenerator.grid)).to.be.true;
+            expect(CrosswordChecker.verify(crosswordGenerator)).to.be.true;
         });
+    });
+
+    describe('getWords()', () => {
+        it('should get all the words parsed from the crossword', () => {
+            crosswordGenerator.generateCrossword('easy');
+            const words = Array.from(crosswordGenerator.words);
+            const parsedWords = CrosswordChecker.getWords(crosswordGenerator, true);
+            expect(words.map((word) => {
+                return parsedWords.has(word);
+            }).reduce((prev, cur) => {
+                return prev || cur;
+            })).to.be.true;
+        });
+    });
+
+    describe('getAllWords()', () => {
+        
     });
 });
--- Cadriciel/server/app/crossword-checker.ts
+++ Cadriciel/server/app/crossword-checker.ts
@@ -1,5 +1,21 @@
+import { CrosswordGenerator } from './crossword';
+
 export module CrosswordChecker {
-    export function verify(grid: string[][]): boolean {
+    export function verify(crossword: CrosswordGenerator): boolean {
         return true;
+    }
+
+    export function getWords(crossword: CrosswordGenerator, horizontal: boolean): Set<string> {
+        const separators = [' ', '#'];
+        // Extracts words from grid by joining the squares, then splitting
+        return new Set(crossword.grid.map((line, index) => {
+            return crossword.patternForLine(index, true)
+                .split(new RegExp(separators.join('|'), 'g'))
+                .filter((value) => {
+                    return value !== '';
+                });
+        }).reduce((prev, cur) => {
+            return prev.concat(cur);
+        }));
     }
 }
