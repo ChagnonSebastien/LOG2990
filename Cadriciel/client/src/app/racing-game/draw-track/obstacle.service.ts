e2cf071ce9f37754dc37f77dd0ca992aa169191f
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2017-10-11 11:35:30 -0400

    started populating addObstacle in obstacle service

--- Cadriciel/client/src/app/racing-game/draw-track/obstacle.service.ts
+++ Cadriciel/client/src/app/racing-game/draw-track/obstacle.service.ts
@@ -11,7 +11,38 @@ export class ObstacleService {
     constructor() {}
 
     public addObstacle(type: ObstacleType) {
+        switch (type) {
+            case ObstacleType.Booster:
+            this.addObstacleToList(this.boosters, type);
+            break;
 
+            case ObstacleType.Pothole:
+            this.addObstacleToList(this.potholes, type);
+            break;
+
+            case ObstacleType.Puddle:
+            this.addObstacleToList(this.puddles, type);
+            break;
+        }
+    }
+
+    private addObstacleToList(list: Obstacle[], type: ObstacleType) {
+        switch (list.length) {
+        case 1:
+        case 3:
+            list.push(this.newObstacle(type));
+        /* falls through */
+        case 0:
+            list.push();
+            break;
+        default:
+            list = [];
+            break;
+        }
+    }
+
+    private newObstacle(type: ObstacleType): Obstacle {
+        return null;
     }
 
     public randomizeAllPositions(type: ObstacleType) {
