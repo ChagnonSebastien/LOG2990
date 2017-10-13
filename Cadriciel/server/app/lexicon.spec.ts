6315234a71dcae38ab45a8b9f062ab670fd15202
Eric Li <sida.li@polymtl.ca>
2017-10-13 11:47:49 -0400

    [test: lexicon.spec.ts] wordsMatching() returns only common or uncommon

--- Cadriciel/server/app/lexicon.spec.ts
+++ Cadriciel/server/app/lexicon.spec.ts
@@ -123,10 +123,16 @@ describe('Lexicon', () => {
         it('should get all the common words matching "h   o"', () => {
             const wordsThatMatch: Array<string> = lexicon.wordsMatching('h   o', true);
             expect(wordsThatMatch.includes('hello')).to.be.true;
+            expect(wordsThatMatch.includes('hydro')).to.be.false;
+            expect(wordsThatMatch.includes('hullo')).to.be.false;
+            expect(wordsThatMatch.includes('hallo')).to.be.false;
+            expect(wordsThatMatch.includes('hollo')).to.be.false;
+            expect(wordsThatMatch.includes('hippo')).to.be.false;
         });
 
         it('should get all the uncommon words matching "h   o"', () => {
             const wordsThatMatch: Array<string> = lexicon.wordsMatching('h   o', false);
+            expect(wordsThatMatch.includes('hello')).to.be.false;
             expect(wordsThatMatch.includes('hydro')).to.be.true;
             expect(wordsThatMatch.includes('hullo')).to.be.true;
             expect(wordsThatMatch.includes('hallo')).to.be.true;
