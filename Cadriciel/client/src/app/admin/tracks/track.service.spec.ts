5ec6181cad925032ed5cb34e45cf70b44ab1c231
Eric Li <sida.li@polymtl.ca>
2017-10-14 12:22:35 -0400

    [lint: admin/tracks]

--- Cadriciel/client/src/app/admin/tracks/track.service.spec.ts
+++ Cadriciel/client/src/app/admin/tracks/track.service.spec.ts
@@ -1,7 +1,5 @@
 import { TestBed, inject } from '@angular/core/testing';
-
 import { AppModule } from '../../app.module';
-
 import { TrackService } from './track.service';
 
 describe('TrackService', () => {
--- Cadriciel/client/src/app/admin/tracks/track.spec.ts
+++ Cadriciel/client/src/app/admin/tracks/track.spec.ts
@@ -3,7 +3,7 @@ import { Track } from './track';
 describe('Track', () => {
     const track = new Track(1, 'a', 'b', 'c');
 
-    it('should be created', function() {
+    it('should be created', function () {
         expect(track).toBeDefined();
     });
 
