dd607c7cba29fab07c817a7195368251ad3d56e6
Julio <julio-herve.dandjinou@polymtl.ca>
2017-10-14 15:37:24 -0400

    admin.component tests with mockAuthencateService are completed

--- Cadriciel/client/src/app/admin/admin.component.spec.ts
+++ Cadriciel/client/src/app/admin/admin.component.spec.ts
@@ -17,10 +17,14 @@ describe('AdminComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [AdminComponent, DrawTrackComponent, TracksComponent],
-            providers: [AuthenticationService, DrawTrackService, TrackValidationService],
             imports: [HttpModule, AdminRoutingModule]
-        })
-            .compileComponents();
+        }).overrideComponent(AdminComponent, {
+            set: {
+                providers: [
+                    { provide: AuthenticationService, useClass: MockAuthenticationService }
+                ]
+            }
+        });
     }));
 
     beforeEach(() => {
@@ -35,19 +39,49 @@ describe('AdminComponent', () => {
 
     it('should login when the password is walleandtomato', () => {
         component.login('walleandtomato').then(res => {
-            expect(res).toBeTruthy();
+            expect(res).toBe(true);
         });
     });
 
     it('should not login when the password is not walleandtomato', () => {
         component.login('blablabla').then(res => {
-            expect(res).toBeFalsy();
+            expect(res).toBe(false);
         });
     });
 
     it('should change the password when old password is walleandtomato', () => {
         component.changePassword('walleandtomato', 'walleandtomato').then(res => {
-            expect(res).toBeTruthy();
+            expect(res).toBe(true);
+        });
+    });
+
+    it('should not change the password when old password is walleandtomato', () => {
+        component.changePassword('blablabla', 'walleandtomato').then(res => {
+            expect(res).toBe(false);
         });
     });
 });
+
+class MockAuthenticationService {
+    public authenticate(password: string): Promise<string> {
+        let result;
+        if (password === 'walleandtomato') {
+            result = 'authenticated';
+        } else {
+            result = 'invalid';
+        }
+       return Promise.resolve(result);
+    }
+
+    public changePassword(oldPassword: string, newPassword: string): Promise<string> {
+        let result;
+        if (oldPassword === 'walleandtomato') {
+            result = 'success';
+        } else {
+            result = 'invalid';
+        }
+        return Promise.resolve(result);
+    }
+}
+
+
