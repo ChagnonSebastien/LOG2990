272050f7cb4c26c7e5a493ec27768c8898ca6d46
Eric <sida.li@polymtl.ca>
2017-10-21 13:55:31 -0400

    [feature: crossword.ts] gridWords initialization

--- Cadriciel/client/src/app/crossword-game/crossword.spec.ts
+++ Cadriciel/client/src/app/crossword-game/crossword.spec.ts
@@ -47,5 +47,8 @@ fdescribe('crossword object', () => {
 
     fit('should construct', () => {
         const crossword = new Crossword(grid, wordsWithIndex, listOfWords);
+        crossword.gridWords.map((line) => {
+            console.log(line);
+        });
     });
 });
--- Cadriciel/client/src/app/crossword-game/crossword.ts
+++ Cadriciel/client/src/app/crossword-game/crossword.ts
@@ -1,6 +1,7 @@
 import { Word } from '../../../../commun/word';
 
 export class Crossword {
+    private size: number;
     public grid: string[][];
     public wordsWithIndex: Array<Word>;
     public foundWords: Array<Word>;
@@ -9,6 +10,7 @@ export class Crossword {
     public gridWords: Array<string>[][];
 
     constructor(grid: string[][], wordsWithIndex: Array<Word>, listOfWords: Array<string>) {
+        this.size = grid.length;
         this.grid = grid;
         this.wordsWithIndex = wordsWithIndex;
         this.foundWords = new Array<Word>();
@@ -20,5 +22,31 @@ export class Crossword {
             map[obj] = obj.length;
             return map;
         }, new Map<string, number>());
+        this.gridWords = this.initializeGridWords();
+    }
+
+    private initializeGridWords(): Array<string>[][] {
+        return this.wordsWithIndex.reduce((gridWords, word) => {
+            return this.insertWordIntoGridWords(gridWords, word);
+        }, this.blankGridWords());
+    }
+
+    private blankGridWords(): Array<string>[][] {
+        return new Array(this.size).fill(undefined).map(res => {
+            return new Array(this.size).fill(undefined).map(u => {
+                return new Array<string>();
+            });
+        });
+    }
+
+    private insertWordIntoGridWords(gridWords: Array<string>[][], word: Word): Array<string>[][] {
+        for (let k = 0; k < word.word.length; k++) {
+            if (word.horizontal) {
+                gridWords[word.i][word.j + k].push(word.word);
+            } else {
+                gridWords[word.i + k][word.j].push(word.word);
+            }
+        }
+        return gridWords;
     }
 }
