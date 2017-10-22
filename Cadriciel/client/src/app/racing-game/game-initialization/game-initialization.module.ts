eb68a0c21d726bcd7c696404ab96d0850d51780a
Audrey Labrie <audrey.labrie@polymtl.ca>
2017-10-21 20:07:37 -0400

    fixed tslint

--- Cadriciel/client/src/app/racing-game/game-initialization/game-initialization.module.ts
+++ Cadriciel/client/src/app/racing-game/game-initialization/game-initialization.module.ts
@@ -5,8 +5,6 @@ import { HttpModule } from '@angular/http';
 
 import { TrackListComponent } from './track-list.component';
 import { TrackDetailComponent } from './track-detail.component';
-import { Track } from '../track';
-import { } from './game-initialization.component';
 
 import { TrackService } from './track.service';
 
