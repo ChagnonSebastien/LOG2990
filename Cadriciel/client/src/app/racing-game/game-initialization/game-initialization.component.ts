b8fa4016ecaebe1052d77e06c4e379f9c1f67ba0
Audrey Labrie <audrey.labrie@polymtl.ca>
2017-10-23 18:39:59 -0400

    added player component

---
+++ Cadriciel/client/src/app/racing-game/game-initialization/game-initialization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-game-initialization-component',
+    templateUrl: './game-initialization.component.html',
+    styleUrls: ['./game-initialization.component.css']
+})
+export class GameInitializationComponent implements OnInit {
+
+    constructor() { }
+
+    public ngOnInit(): void {
+    }
+
+}
