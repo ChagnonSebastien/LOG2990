4ef5c42bb6382716a4fd472235e613a8179c712f
Unknown <chagnon.s21@gmail.com>
2017-10-24 15:57:23 -0400

    Added the saveTrack tests in the drawTrackService

--- Cadriciel/client/src/app/racing-game/draw-track/draw-track.service.spec.ts
+++ Cadriciel/client/src/app/racing-game/draw-track/draw-track.service.spec.ts
@@ -1,9 +1,14 @@
+import { DrawTrackComponent } from './draw-track.component';
+import { Obstacle, ObstacleType } from './obstacle';
+import { Track } from './../../admin/tracks/track';
+import { MockBackend } from '@angular/http/testing';
+import { TestBed, async, fakeAsync, tick, inject, ComponentFixture } from '@angular/core/testing';
 import { ObstacleService } from './obstacle.service';
 import { RenderService } from './render.service';
 import { TrackValidationService } from './track-validation.service';
 import { DrawTrackService } from './draw-track.service';
 import {
-    Http,
+    Http, Response,
     BrowserXhr,
     RequestOptions,
     BaseRequestOptions,
@@ -17,53 +22,63 @@ import {
 import { ReflectiveInjector } from '@angular/core';
 import * as THREE from 'three';
 
+
+
 describe('test drawTrackService', function () {
-    let drawTrackService: DrawTrackService;
-    beforeEach(() => {
-        const injector = ReflectiveInjector.resolveAndCreate([
-            Http,
-            BrowserXhr,
+
+    const renderServiceStub = {};
+    const trackValidationServiceStub = {};
+    const obstacleServiceStub = {
+        getObstacles(type: ObstacleType): Obstacle[] {
+            return [];
+        }
+    };
+
+    beforeEach(async(() => {
+        this.injector = ReflectiveInjector.resolveAndCreate([
+            { provide: ConnectionBackend, useClass: MockBackend },
             { provide: RequestOptions, useClass: BaseRequestOptions },
-            { provide: ResponseOptions, useClass: BaseResponseOptions },
-            { provide: ConnectionBackend, useClass: XHRBackend },
-            { provide: XSRFStrategy, useFactory: () => new CookieXSRFStrategy() },
+            { provide: TrackValidationService, useValue: trackValidationServiceStub },
+            { provide: RenderService, useValue: renderServiceStub },
+            { provide: ObstacleService, useValue: obstacleServiceStub },
+            Http,
+            DrawTrackService
         ]);
-        const http = injector.get(Http);
-        drawTrackService = new DrawTrackService(new RenderService(), new TrackValidationService(), new ObstacleService(), http);
+        this.drawTrackService = this.injector.get(DrawTrackService);
+        this.backend = this.injector.get(ConnectionBackend) as MockBackend;
+        this.backend.connections.subscribe((connection: any) => this.lastConnection = connection);
+    }));
+
+    it('should be created', () => {
+        expect(this.drawTrackService).toBeTruthy();
     });
 
-    it('construction test', done => {
-        expect(drawTrackService).toBeTruthy();
-        done();
+    it('should be able to definie if loop is closed', () => {
+        this.drawTrackService.trackClosed = false;
+        expect(this.drawTrackService.isFinished()).toEqual(false);
     });
 
-    it('Able to definie if loop is closed', done => {
-        drawTrackService.trackClosed = false;
-        expect(drawTrackService.isFinished()).toEqual(false);
-        done();
-    });
-
-    it('Calculate distance between two points', done => {
+    it('should be able to calculate distance between two points', () => {
         const vector1 = new THREE.Vector2(1, 1);
         const vector2 = new THREE.Vector2(1, 2);
-        expect(drawTrackService.getXYDistance(vector1, vector2)).toEqual(1);
-        done();
+        expect(this.drawTrackService.getXYDistance(vector1, vector2)).toEqual(1);
     });
 
-    it('Calculates the relative position', done => {
+    it('should be able to calculates the relative position', () => {
         const vector1 = new THREE.Vector2(1, 1);
         const vector2 = new THREE.Vector2(1, 2);
-        expect(drawTrackService.getXYDistance(vector1, vector2)).toEqual(1);
-        done();
+        expect(this.drawTrackService.getXYDistance(vector1, vector2)).toEqual(1);
     });
 
-    describe('saveTrack()', () => {
-        it('should save a track in the database', (done) => {
-            drawTrackService.saveTrack('test', 'testing', 'advanced')
-            .then((result) => {
-                expect(result).toEqual('success');
-                done();
-            });
-        });
-    });
+    it('should be able to post to the server a track and receive a response', fakeAsync(() => {
+        let result: String;
+        const trackToSave = new Track( 0, 'a', 'b', 'c', [], [], [], []);
+
+        this.drawTrackService.saveTrack('name', 'description', 'difficulty').then((serverResponse: String) => result = serverResponse);
+        this.lastConnection.mockRespond(new Response(new ResponseOptions({
+            body: JSON.stringify({ data: 'success' }),
+        })));
+        tick();
+        expect(result).toBe('success');
+    }));
 });
