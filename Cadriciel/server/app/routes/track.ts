2d9920bf6fc88f89c5d3a55413ce90b489cb6a14
Sebastien Chagnon <chagnon.s21@gmail.com>
2017-10-24 22:28:15 -0400

    the server now returns the track to the client

--- Cadriciel/server/app/routes/track.ts
+++ Cadriciel/server/app/routes/track.ts
@@ -23,6 +23,18 @@ module Route {
             });
         }
 
+        public getTrack(req: express.Request, res: express.Response, next: express.NextFunction) {
+            MongoClient.connect(url, (err, db) => {
+                if (err) {
+                    res.send(JSON.stringify({ 'data': 'connectionError' }));
+                } else {
+                    db.collection('tracks').findOne({ _id: req.params.id }).then((track) => {
+                        res.send(JSON.stringify(track));
+                    });
+                }
+            });
+        }
+
         public deleteTrack(req: express.Request, res: express.Response, next: express.NextFunction) {
             MongoClient.connect(url, (err, db) => {
                 if (err) {
