f46bd8c392aa46e1fed111bf0a504b5a57f08e05
Sebastien Chagnon <chagnon.s21@gmail.com>
2017-10-27 03:30:19 -0400

    The delete button now deletes the track

--- Cadriciel/client/src/app/racing-game/game-initialization/track-detail.component.ts
+++ Cadriciel/client/src/app/racing-game/game-initialization/track-detail.component.ts
@@ -1,7 +1,10 @@
+import { Http } from '@angular/http';
 import { Component, OnInit, Input } from '@angular/core';
 import { Track } from '../track';
 import { TrackService } from './track.service';
 import { User } from './user';
+
+const apiUrl = 'http://localhost:3000/api';
 
 @Component({
     selector: 'app-track-detail',
@@ -11,7 +14,7 @@ import { User } from './user';
 })
 export class TrackDetailComponent implements OnInit {
     @Input() public userType: User;
-    @Input() public  track: Track;
+    @Input() public track: Track;
 
     public changeDescriptionDB() {
         this.trackService.changeTrackDescription(this.track.name, this.track.description).subscribe(
@@ -34,11 +37,28 @@ export class TrackDetailComponent implements OnInit {
         this.changeTypeDB();
     }
 
+    public delete(): Promise<string> {
+        const path = 'track';
+        return this.http
+            .delete(`${apiUrl}/${path}/${this.track.name}`
+        ).toPromise()
+        .then(response => response.json().data)
+        .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
+
     public setTrack(track: Track) {
         this.track = track;
     }
 
-    constructor(private trackService: TrackService) { }
+    constructor(
+        private trackService: TrackService,
+        private http: Http
+    ) { }
 
     public ngOnInit() {
     }
