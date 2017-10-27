aeddefaa9045c312021a447e1739ab7e7d58eb56
Julio <julio-herve.dandjinou@polymtl.ca>
2017-10-27 10:57:08 -0400

    tslint

--- Cadriciel/client/src/app/app-routing.module.ts
+++ Cadriciel/client/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RacingGameComponent } from './racing-game/racing-game.component';
 import { PlayerComponent } from './racing-game/game-initialization/player.component';
-import { TrackDetailComponent } from './racing-game/game-initialization/track-detail.component';
 import { DrawTrackComponent } from './racing-game/draw-track/draw-track.component';
 
 const appRoutes: Routes = [
