2bb05e56f8d259f42f47ebaaf2024e00a59e2c45
Eric <sida.li@polymtl.ca>
2017-10-27 14:25:31 -0400

    [refactor] crossword-hints.service now has info on words

--- Cadriciel/client/src/app/crossword-game/crossword-hints.service.ts
+++ Cadriciel/client/src/app/crossword-game/crossword-hints.service.ts
@@ -8,10 +8,14 @@ import { Word } from '../../../../commun/word';
 @Injectable()
 export class CrosswordHintsService {
     public hints: Array<Hint>;
+    public wordsWithIndex: Array<Word>;
+    public wordMap: Map<string, Word>;
 
     constructor(private lexiconService: LexiconService) { }
 
     public newGame(wordsWithIndex: Array<Word>) {
+        this.wordsWithIndex = wordsWithIndex;
+        this.wordMap = this.constructWordMap();
         this.hints = new Array<Hint>();
         for (const word of wordsWithIndex) {
             this.lexiconService.getWordDefinition(word.word)
@@ -19,5 +23,17 @@ export class CrosswordHintsService {
                     this.hints.push(new Hint(word.word, definition));
                 });
         }
+    }
+
+    public getWordInfo(word: string): Word {
+        return this.wordMap.get(word);
+    }
+
+    // Provide O(1) access to information on a word.
+    private constructWordMap(): Map<string, Word> {
+        return this.wordsWithIndex.reduce((map, obj) => {
+            map.set(obj.word, obj);
+            return map;
+        }, new Map<string, Word>());
     }
 }
