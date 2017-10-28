57f41251e022ff7e7872aa40a1ea71c0d3c7445d
Eric <sida.li@polymtl.ca>
2017-10-28 17:35:29 -0400

    [crossword-points.service] added methods newGame(), wordFound(), wordHasBeenFound()

--- Cadriciel/client/src/app/crossword-game/crossword-points.service.ts
+++ Cadriciel/client/src/app/crossword-game/crossword-points.service.ts
@@ -2,5 +2,19 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CrosswordPointsService {
+    private foundWords: Set<string>;
 
+    constructor() { }
+
+    public newGame() {
+        this.foundWords = new Set<string>();
+    }
+
+    public foundWord(word: string) {
+        this.foundWords.add(word);
+    }
+
+    public wordHasBeenFound(word: string): boolean {
+        return this.foundWords.has(word);
+    }
 }
