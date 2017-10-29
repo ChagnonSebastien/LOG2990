e1e94e0a5e59a0e1c7efea48bea4ccdc250ea404
Eric <sida.li@polymtl.ca>
2017-10-28 21:04:15 -0400

    [REFACTOR FILE STRUCTURE] Created CrosswordRoutingModule

--- Cadriciel/client/src/app/crossword-game/crossword.module.ts
+++ Cadriciel/client/src/app/crossword-game/crossword.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { CrosswordRoutingModule } from './crossword-routing.module';
+
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
-        HttpModule
+        HttpModule,
+        CrosswordRoutingModule
     ]
 })
 export class CrosswordModule { }
