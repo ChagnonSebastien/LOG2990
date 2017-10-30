95bb570fd18eabbb8ff619b14f84b7b1408da30a
Sebastien Chagnon <chagnon.s21@gmail.com>
2017-10-30 17:17:33 -0400

    Fixed a lint error

--- Cadriciel/client/src/app/racing-game/game-initialization/player.component.ts
+++ Cadriciel/client/src/app/racing-game/game-initialization/player.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { User } from './user';
-
 @Component({
     selector: 'app-player-component',
     templateUrl: './player.component.html',
