868b673c655c1a9d834865e99f75a264ec0a292e
Sebastien Chagnon <chagnon.s21@gmail.com>
2017-11-01 16:14:39 -0400

    Added pupulate() to the TerrainGeneration service and it's scale constant

--- Cadriciel/client/src/app/racing-game/terrain-generation.service.ts
+++ Cadriciel/client/src/app/racing-game/terrain-generation.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@angular/core';
 import * as THREE from 'three';
 
+const scale = 1;
+
 @Injectable()
 export class TerrainGenerationService {
+
+    constructor() {
+
+    }
+
+    public populate(scene: THREE.Scene): void {
+
+    }
 
 }
