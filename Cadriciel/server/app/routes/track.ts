240cee4e624620862f5afa4399e2a56677a2711c
Julio <julio-herve.dandjinou@polymtl.ca>
2017-11-01 11:28:07 -0400

    update endGameUpdate

--- Cadriciel/server/app/routes/track.ts
+++ Cadriciel/server/app/routes/track.ts
@@ -73,24 +73,24 @@ module Route {
             });
 
         }
-
+        
         public updateRating (numberOfTimesPlayed: number, oldRating: number, newRating: number): number {
-             return (numberOfTimesPlayed * oldRating + newRating ) / (numberOfTimesPlayed + 1);
+             return (numberOfTimesPlayed * oldRating + newRating ) / (numberOfTimesPlayed + 1);  
         }
 
-        public updateBestTimes (arrayBestTimes: number[], newtime: number ) {
+        public updateBestTimes (arrayBestTimes: number[], newtime: number ) { 
             const fifthBestTimes = 5;
-            arrayBestTimes.sort((a, b) => {
+            arrayBestTimes.sort((a, b) => { 
                   return a - b;
-            });
+            })
               arrayBestTimes = arrayBestTimes.slice(0, fifthBestTimes);
-              for ( let time = 0; time < arrayBestTimes.length - 1; time++) {
-                if (newtime < arrayBestTimes[time]) {
+              for( let time = 0; time < arrayBestTimes.length - 1; time++) {
+                if(newtime < arrayBestTimes[time]){
                     arrayBestTimes[time + 1] = arrayBestTimes[time];
                     arrayBestTimes[time] = newtime;
                     return arrayBestTimes;
                 }
-              }
+              }             
         }
 
         public endGameUpdate(req: express.Request, res: express.Response, next: express.NextFunction) {
@@ -102,25 +102,25 @@ module Route {
                 if (err) {
                     res.send(JSON.stringify({ 'data': 'connectionError' }));
                 } else {
-                    db.collection('track').findOne({id: req.params.update.name })
+                    db.collection('track').findOne({ _id: req.params.update.name })
                         .then((trackDB) => {
                             tempRating = this.updateRating(req.body.numberOfTimesPlayed, trackDB.rating, req.body.rating );
-                            tempBestTimes = this.updateBestTimes(trackDB.bestTimes, req.body.t);
+                            tempBestTimes = this.updateBestTimes(trackDB.bestTimes, req.body.time);
                             tempNbOfTimesPlayed = trackDB.numberOfTimesPlayed++;
-
+                            
                             db.collection('tracks').update(
-                                { id: req.params.update.name },
+                                { id: req.params.update.name }, 
                                 { rating: tempRating,
                                   bestTimes: tempBestTimes,
                                   numberOfTimePlayed: tempNbOfTimesPlayed },
                                 { upsert: false
-                                });
-                        });
+                                });  
+                        })                
                 }
-
+                 
             });
         }
-
+        
     }
 }
 
