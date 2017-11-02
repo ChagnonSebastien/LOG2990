9742c317a3cdce08346329c9c0d54431f9234a42
Eric <sida.li@polymtl.ca>
2017-11-02 14:44:45 -0400

    [testing] skeleton, testbed for hints-service.spec.ts

---
+++ Cadriciel/client/src/app/crossword-game/crossword-hints/crossword-hints.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CrosswordHintsService } from './crossword-hints.service';
+import { LexiconService } from '../lexicon.service';
+import { CrosswordPointsService } from '../crossword-points/crossword-points.service';
+
+describe('#CrosswordHintsService', () => {
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+
+        });
+    });
+});
