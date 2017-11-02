fe338dab45027993136b28fe552b43870204c5ca
Eric <sida.li@polymtl.ca>
2017-11-02 14:57:55 -0400

    [testing hints.service] should construct

--- Cadriciel/client/src/app/crossword-game/crossword-hints/crossword-hints.service.spec.ts
+++ Cadriciel/client/src/app/crossword-game/crossword-hints/crossword-hints.service.spec.ts
@@ -1,14 +1,40 @@
 import { TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs/Rx';
+import { HttpModule } from '@angular/http';
 
 import { CrosswordHintsService } from './crossword-hints.service';
-import { LexiconService } from '../lexicon.service';
 import { CrosswordPointsService } from '../crossword-points/crossword-points.service';
+import { CrosswordGridService } from '../crossword-grid/crossword-grid.service';
+import { LexiconService } from '../lexicon.service';
+
+class MockLexiconService extends LexiconService {
+    public getWordDefinitions(words: Array<string>): Observable<any> {
+        return Observable.forkJoin(
+            words.map((word) => {
+                return Promise.resolve(`Definition of ${word}`);
+            })
+        );
+    }
+}
+
+let hintsService: CrosswordHintsService;
 
 describe('#CrosswordHintsService', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-
+            imports: [HttpModule],
+            providers: [
+                CrosswordHintsService,
+                CrosswordPointsService,
+                CrosswordGridService,
+                { provide: LexiconService, useClass: MockLexiconService }
+            ]
         });
+        hintsService = TestBed.get(CrosswordHintsService);
+    });
+
+    it('should construct', () => {
+        expect(hintsService).toBeDefined();
     });
 });
