6bb34a120011006c8e246ec7996e7c68d0222c38
Audrey Labrie <audrey.labrie@polymtl.ca>
2017-11-03 10:37:19 -0400

    added shadow

--- Cadriciel/client/src/app/racing-game/vehicle.ts
+++ Cadriciel/client/src/app/racing-game/vehicle.ts
@@ -20,14 +20,14 @@ export class Vehicle {
         await loader.load('/assets/cart.json', (object: THREE.Object3D) => {
             console.log('z: ', object);
             this.vehicle = <THREE.Mesh>object;
-            this.vehicle.geometry.rotateY(Math.PI / 2);
+            this.vehicle.geometry.rotateY(Math.PI / 2); // So that the front of the cart is oriented correctly in the scene
             this.vehicle.position.setX(0);
             this.vehicle.position.setY(30);
             this.vehicle.position.setZ(0);
             this.vehicle.scale.setX(22);
             this.vehicle.scale.setY(22);
             this.vehicle.scale.setZ(22);
-            this.vehicle.rotation.y = VEHICLE_ROTATION_Y;
+            this.vehicle.castShadow = true;
             console.log('z: ', this.vehicle);
             this.vehicleSubject.next('created');
         }, (object: any) => {
