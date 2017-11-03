a5c45b8227881c3421ac7824dba554edfd37c697
Audrey Labrie <audrey.labrie@polymtl.ca>
2017-11-03 14:45:30 -0400

    racinggameservice test: should be alerted when main vehicle initialized

--- Cadriciel/client/src/app/racing-game/racing-game.service.spec.ts
+++ Cadriciel/client/src/app/racing-game/racing-game.service.spec.ts
@@ -2,6 +2,8 @@ import { RacingGameService } from './racing-game.service';
 import { RacingGameModule } from './racing-game.module';
 import { TestBed, inject } from '@angular/core/testing';
 
+let racingGameService;
+
 describe('RacingGameService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -10,10 +12,18 @@ describe('RacingGameService', () => {
             ],
             providers: [RacingGameService]
         });
+        racingGameService = TestBed.get(RacingGameService);
     });
 
     it('should be created', inject([RacingGameService], (service: RacingGameService) => {
         expect(service).toBeTruthy();
     }));
+
+    it('should be alerted when main vehicle is initialized', () => {
+        racingGameService.vehicleAlerts().subscribe((vehicle) => {
+            expect(vehicle).toBeTruthy();
+        });
+    });
+
 });
 
