a122e762001bda49f7a9667c31ac0995c1888fa1
Audrey Labrie <audrey.labrie@polymtl.ca>
2017-11-03 12:28:44 -0400

    adde observable for opponents vehicle

--- Cadriciel/client/src/app/racing-game/racing-game.service.ts
+++ Cadriciel/client/src/app/racing-game/racing-game.service.ts
@@ -2,34 +2,72 @@ import { Injectable } from '@angular/core';
 import { Vehicle } from './vehicle';
 import { Subject } from 'rxjs/Subject';
 import {Observable} from 'rxjs/Observable';
+
+const numberOfOpponents = 3;
 @Injectable()
 export class RacingGameService {
-    public vehicle: Vehicle;
+    public mainVehicle: Vehicle;
+    public opponentsVehicles: Array<Vehicle>;
     private vehicleSubject: Subject<any>;
+    private opponentsSubject: Subject<any>;
+    public numberOfVehiclesInitialized: number;
 
     constructor() {
         this.vehicleSubject = new Subject();
-        this.vehicle = new Vehicle();
+        this.mainVehicle = new Vehicle();
+        this.opponentsVehicles = [];
+        this.numberOfVehiclesInitialized = 0;
+        for (let i = 0; i < numberOfOpponents; i++) {
+            this.opponentsVehicles[i] = new Vehicle();
+        }
         this.alertWhenVehicleIsInitialized();
         this.initializeVehicle();
+        this.initializeOpponentsVehicles();
     }
 
     public async initializeVehicle() {
-        this.vehicle.createVehicle();
+
+        this.mainVehicle.createVehicle(300, 30, 0);
+    }
+
+    private initializeOpponentsVehicles() {
+        console.log('opnnents initializing');
+        this.opponentsVehicles[0].createVehicle(200, 30, 0);
     }
 
     public vehicleAlerts(): Observable<any> {
         return this.vehicleSubject.asObservable();
     }
 
+    public opponentsAlerts(): Observable<any> {
+        console.log('second vehicle init');
+        return this.opponentsSubject.asObservable();
+    }
+
     private alertVehicle() {
-        this.vehicleSubject.next(this.vehicle.vehicle);
+        this.vehicleSubject.next(this.mainVehicle.vehicle);
+    }
+
+    private alertOpponentsVehicles() {
+        console.log('ALERT OPPONENTS VEHICLES');
+        this.opponentsSubject.next(this.opponentsVehicles);
     }
 
     private alertWhenVehicleIsInitialized() {
-        this.vehicle.vehicleAlert().subscribe((result) => {
-            this.alertVehicle();
+        this.mainVehicle.vehicleAlert().subscribe((result) => {
+            this.numberOfVehiclesInitialized++;
+            if (this.numberOfVehiclesInitialized === 1) {
+                this.alertVehicle();
+            } else if (this.numberOfVehiclesInitialized === 2) {
+                this.alertOpponentsVehicles();
+            }
         });
     }
+
+    private alertWhenOpponentsAreInitialized() {
+
+    }
+
+
 
 }
--- Cadriciel/client/src/app/racing-game/vehicle.ts
+++ Cadriciel/client/src/app/racing-game/vehicle.ts
@@ -15,19 +15,20 @@ export class Vehicle {
         return this.vehicleSubject.asObservable();
     }
 
-    public async createVehicle() {
+    public async createVehicle(x: number, y: number, z: number) {
         const loader = new THREE.ObjectLoader();
         await loader.load('/assets/cart.json', (object: THREE.Object3D) => {
             console.log('z: ', object);
             this.vehicle = <THREE.Mesh>object;
             this.vehicle.geometry.rotateY(Math.PI / 2); // So that the front of the cart is oriented correctly in the scene
-            this.vehicle.position.setX(0);
-            this.vehicle.position.setY(30);
-            this.vehicle.position.setZ(0);
+            this.vehicle.position.setX(x);
+            this.vehicle.position.setY(y);
+            this.vehicle.position.setZ(z);
             this.vehicle.scale.setX(22);
             this.vehicle.scale.setY(22);
             this.vehicle.scale.setZ(22);
             this.vehicle.castShadow = true;
+            console.log('x: ', x);
             console.log('z: ', this.vehicle);
             this.vehicleSubject.next('created');
         }, (object: any) => {
