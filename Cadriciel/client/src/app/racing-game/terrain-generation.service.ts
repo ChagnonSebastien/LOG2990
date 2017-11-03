e593e954d3ee161564009bc88457f12f30350a97
Unknown <chagnon.s21@gmail.com>
2017-11-03 19:34:05 -0400

    Added method generateTable to terrainGenerationService that adds the table to the scene

--- Cadriciel/client/src/app/racing-game/terrain-generation.service.ts
+++ Cadriciel/client/src/app/racing-game/terrain-generation.service.ts
@@ -5,23 +5,44 @@ import * as THREE from 'three';
 @Injectable()
 export class TerrainGenerationService {
 
+    private track: Track;
+
+    private textureSky: any;
+
+    private scale: number;
+
     constructor() {
 
     }
 
-    public generate(scene: THREE.Scene, track: Track): void {
+    public generate(scene: THREE.Scene, track: Track, scale: number): void {
+        this.scale = scale;
         const url = '../../assets/images/skybox/';
         const images = [url + 'xpos.png', url + 'xneg.png',
         url + 'ypos.png', url + 'yneg.png',
         url + 'zpos.png', url + 'zneg.png'];
-        const textureSky = THREE.ImageUtils.loadTextureCube(images);
-        const tableMaterial = new THREE.MeshStandardMaterial ( {color: 0xF0F0F0, roughness: 0, metalness: 0, envMap: textureSky} );
-        const tableGeometry = new THREE.PlaneGeometry(1000, 1000, 1, 1);
+        this.textureSky = THREE.ImageUtils.loadTextureCube(images);
+        scene.add(this.generateTable(track));
+    private generateTable(track: Track): THREE.Mesh {
+        const maximumX = Math.max.apply(null, track.trackIntersections.map(intersection => intersection.x));
+        const minimumX = Math.min.apply(null, track.trackIntersections.map(intersection => intersection.x));
+        const maximumY = Math.max.apply(null, track.trackIntersections.map(intersection => intersection.y));
+        const minimumY = Math.min.apply(null, track.trackIntersections.map(intersection => intersection.y));
+
+        const tableMaterial = new THREE.MeshStandardMaterial ( {color: 0xF0F0F0, roughness: 0, metalness: 0, envMap: this.textureSky} );
+        const tableGeometry = new THREE.PlaneGeometry(
+            (this.scale * (maximumX - minimumX)) + (this.scale * 40 * 3),
+            (this.scale * (maximumY - minimumY)) + (this.scale * 40 * 3),
+            1,
+            1
+        );
+        tableGeometry.rotateX(Math.PI / -2);
         const table = new THREE.Mesh(tableGeometry, tableMaterial);
-        table.castShadow = true;
+
+        table.position.x = minimumX + ((maximumX - minimumX) / 2);
+        table.position.z = minimumY + ((maximumY - minimumY) / 2);
         table.receiveShadow = true;
-        table.rotateX(Math.PI / -2);
-        scene.add(table);
+        return table;
     }
 
 }
