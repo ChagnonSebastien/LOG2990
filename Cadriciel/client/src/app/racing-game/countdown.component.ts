f32db32ef744fb954e18bccb03fe4387aeab6678
Audrey Labrie <audrey.labrie@polymtl.ca>
2017-11-04 19:47:37 -0400

    added countdown component and service and added to racing game moduel

---
+++ Cadriciel/client/src/app/racing-game/countdown.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-countdown',
+    templateUrl: './countdown.component.html',
+    styleUrls: ['./countdown.component.css'],
+    providers: []
+})
+export class CountdownComponent {
+
+    constructor() {}
+
+}
