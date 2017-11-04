af72a55da2759d66bb91c00db10450e74558fbe4
Sebastien Chagnon <chagnon.s21@gmail.com>
2017-11-04 10:43:57 -0400

    Fixed command service for testing

--- Cadriciel/client/src/app/racing-game/commands.service.ts
+++ Cadriciel/client/src/app/racing-game/commands.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import * as THREE from 'three';
 
+// tslint:disable-next-line
 enum Commands {
     ACCELETATE, TURN_LEFT, TURN_RIGHT, CAMERA_VIEW,
     NIGHT_MODE, COLOR_BLIND_MODE, ZOOM
@@ -9,11 +9,11 @@ enum Commands {
 @Injectable()
 export class CommandsService {
 
-    private keyDownEvent(event) {
+    public keyDownEvent(event) {
 
     }
 
-    private keyUpEvent(event) {
+    public keyUpEvent(event) {
 
     }
 }
