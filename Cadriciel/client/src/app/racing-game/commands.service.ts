8231ac3af3fd8024f861e9949a31b0f703fa9665
Sebastien Chagnon <chagnon.s21@gmail.com>
2017-11-04 10:44:55 -0400

    Added a tsLint rule flag to ignore unused variable in command service

--- Cadriciel/client/src/app/racing-game/commands.service.ts
+++ Cadriciel/client/src/app/racing-game/commands.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-// tslint:disable-next-line
+// tslint:disable-next-line:no-unused-variable
 enum Commands {
     ACCELETATE, TURN_LEFT, TURN_RIGHT, CAMERA_VIEW,
     NIGHT_MODE, COLOR_BLIND_MODE, ZOOM
