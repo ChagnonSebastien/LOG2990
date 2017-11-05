a2d95c92eff8273c90cec0f08b5ebec6033a6ee5
Julio <julio-herve.dandjinou@polymtl.ca>
2017-11-05 03:29:56 -0500

    Implemented tests in updatetrack.spec.ts

--- Cadriciel/server/app/updatetrack.spec.ts
+++ Cadriciel/server/app/updatetrack.spec.ts
@@ -3,6 +3,7 @@ import { UpdateTrack } from './updatetrack';
 
 const arrayBeyondFive: number[] = [12, 5, 7, 9, 4, 19, 15, 3];
 const arrayBelowFive: number[] = [13, 9];
+
 describe('UpdateTrack', () => {
     describe('updateRating', () => {
         it('should calcul a new rating value, to find 4.25 ', () => {
@@ -16,10 +17,11 @@ describe('UpdateTrack', () => {
         });
     });
     describe('updateBesTimes', () => {
-        it('should insert an element in array ', () => {
+        it('should insert 1 in  the array whose lengh is less than 5 ', () => {
             const newArray = UpdateTrack.updateBestTimes(arrayBelowFive, 1);
             expect(newArray.length).to.equal(3);
             expect(newArray[0]).to.equal(1);
+            expect(newArray[1]).to.equal(9);
             expect(newArray[2]).to.equal(13);
         });
 
@@ -27,15 +29,18 @@ describe('UpdateTrack', () => {
             const newArray = UpdateTrack.updateBestTimes(arrayBeyondFive, 12);
             expect(newArray.length).to.equal(5);
             expect(newArray[0]).to.equal(3);
+            expect(newArray[1]).to.equal(4);
+            expect(newArray[2]).to.equal(5);
             expect(newArray[3]).to.equal(7);
+            expect(newArray[4]).to.equal(9);
         });
-        it('should insert 2 in index 0 on the array that lengh is 5', () => {
+        it('should insert 2 to index 0 in the array whose lengh is 5', () => {
             const newArray = UpdateTrack.updateBestTimes(arrayBeyondFive, 2);
             expect(newArray.length).to.equal(5);
             expect(newArray[0]).to.equal(2);
             expect(newArray[4]).to.equal(7);
         });
-        it('should insert 6 in index 4 on the array', () => {
+        it('should insert 6 to index 4 in the array', () => {
             const newArray = UpdateTrack.updateBestTimes(arrayBeyondFive, 6);
             expect(newArray[3]).to.equal(6);
         });
