00cf5d748d39996a14e581b001238cf1e6db74a6
Eric Li <sida.li@polymtl.ca>
2017-11-07 17:42:42 -0500

    [lint] crossword-room.component

--- Cadriciel/client/src/app/crossword-game/crossword-room/crossword-room.component.ts
+++ Cadriciel/client/src/app/crossword-game/crossword-room/crossword-room.component.ts
@@ -4,33 +4,33 @@ import { PlayerManagerService } from '../crossword-player-manager.service';
 import { ClientGameInfo } from '../../../../../commun/crossword/clientGameInfo';
 
 @Component({
-  selector: 'app-crossword-room',
-  templateUrl: './crossword-room.component.html',
-  styleUrls: ['./crossword-room.component.css']
+    selector: 'app-crossword-room',
+    templateUrl: './crossword-room.component.html',
+    styleUrls: ['./crossword-room.component.css']
 })
 export class CrosswordRoomComponent implements OnInit {
 
-  public username: string;
-  public gamesListInfo: ClientGameInfo[] = [];
-  @Output() private startGameEmitter: EventEmitter<any>;
-  @Output() private endGameEmitter: EventEmitter<any>;
-  constructor(private multiplayerService: CrosswordMultiplayerService, private playerManagerService: PlayerManagerService) {
-    this.username = '';
-    this.startGameEmitter = new EventEmitter();
-  }
+    public username: string;
+    public gamesListInfo: ClientGameInfo[] = [];
+    @Output() private startGameEmitter: EventEmitter<any>;
+    @Output() private endGameEmitter: EventEmitter<any>;
+    constructor(private multiplayerService: CrosswordMultiplayerService, private playerManagerService: PlayerManagerService) {
+        this.username = '';
+        this.startGameEmitter = new EventEmitter();
+    }
 
-  public ngOnInit() {
-    this.multiplayerService.getGames().then(games => {
-      this.gamesListInfo = games;
-    });
-  }
+    public ngOnInit() {
+        this.multiplayerService.getGames().then(games => {
+            this.gamesListInfo = games;
+        });
+    }
 
-  public joinGame(gameId: string) {
-    this.setPlayerUsername();
-    this.multiplayerService.joinGame(gameId, this.playerManagerService.getPlayer());
-  }
+    public joinGame(gameId: string) {
+        this.setPlayerUsername();
+        this.multiplayerService.joinGame(gameId, this.playerManagerService.getPlayer());
+    }
 
-  public setPlayerUsername(): void {
-    this.playerManagerService.getPlayer().setUsername(this.username);
-  }
+    public setPlayerUsername(): void {
+        this.playerManagerService.getPlayer().setUsername(this.username);
+    }
 }
