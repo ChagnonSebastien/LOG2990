c74025040d6db02cc80e1fb26a9cf27f449260ee
micheeKamte <micheekamte@gmail.com>
2017-11-07 19:58:50 -0500

    create controls class

---
+++ Cadriciel/client/src/app/racing-game/contols.ts
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+
+export class Controls {
+    private speed: number;
+
+    private acceleration: number;
+
+    private rotationAngle: number;
+
+    constructor(speed: number, acceleration: number, angle: number) {
+        this.speed = speed;
+        this.acceleration = acceleration;
+        this.rotationAngle = angle;
+    }
+
+    public accelerate (object: any) {
+        this.speed += this.acceleration;
+        object.translateZ(this.speed);
+    }
+
+    public brake (object: any) {
+        this.speed -= this.acceleration;
+        object.translateZ(this.speed);
+    }
+
+    public leftRotation(object: any) {
+        object.rotate.x(this.rotationAngle);
+    }
+
+    public rightRotation(object: any) {
+        object.rotate.x(-this.rotationAngle);
+    }
+}
