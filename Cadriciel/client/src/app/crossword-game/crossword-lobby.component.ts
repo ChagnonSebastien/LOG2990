183441280366e6d21a4bfa9095e5857cb735688a
Eric Li <sida.li@polymtl.ca>
2017-11-07 19:58:21 -0500

    added multiplayerService to lobby

--- Cadriciel/client/src/app/crossword-game/crossword-lobby.component.ts
+++ Cadriciel/client/src/app/crossword-game/crossword-lobby.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { CrosswordMultiplayerService } from './crossword-multiplayer.service';
+
 @Component({
     selector: 'app-crossword-lobby',
     templateUrl: './crossword-lobby.component.html',
     styleUrls: ['./crossword-lobby.component.css']
 })
 export class CrosswordLobbyComponent {
+    constructor(private multiplayerService: CrosswordMultiplayerService) {
 
+    }
 }
