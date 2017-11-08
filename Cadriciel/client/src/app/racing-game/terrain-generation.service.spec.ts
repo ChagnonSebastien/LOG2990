c5669497b961c324f4c5cfa4390c99f89b52b3b6
Unknown <chagnon.s21@gmail.com>
2017-11-07 21:46:45 -0500

    Solved yet again tsLint errors

--- Cadriciel/client/src/app/racing-game/terrain-generation.service.spec.ts
+++ Cadriciel/client/src/app/racing-game/terrain-generation.service.spec.ts
@@ -1,8 +1,5 @@
-import { Obstacle, ObstacleType } from './draw-track/obstacle';
-import { Track } from './track';
 import { TerrainGenerationService } from './terrain-generation.service';
 import { TestBed } from '@angular/core/testing';
-import * as THREE from 'three';
 
 let terrainGenerationService;
 
