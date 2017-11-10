03952216a00ac89c9cc7b87c50b1cbf96cf64ad8
Audrey Labrie <audrey.labrie@polymtl.ca>
2017-11-09 22:27:27 -0500

    newGame() tested

--- Cadriciel/client/src/app/crossword-game/words/crossword-words.service.spec.ts
+++ Cadriciel/client/src/app/crossword-game/words/crossword-words.service.spec.ts
@@ -1,5 +1,6 @@
 import { CrosswordWordsService } from './crossword-words.service';
 import { TestBed } from '@angular/core/testing';
+import { Word } from '../../../../../commun/word';
 
 let crosswordWordsService: CrosswordWordsService;
 
@@ -14,4 +15,11 @@ describe('CrosswordWordsService', () => {
     it('should construct', () => {
         expect(crosswordWordsService).toBeDefined();
     });
+
+    it('Should initialize new word map', () => {
+        const word = new Word(0 , 0, 'cat', true);
+        crosswordWordsService.newGame([word]);
+        expect(crosswordWordsService['wordMap'].get('cat')).toEqual(word);
+    });
+
 });
