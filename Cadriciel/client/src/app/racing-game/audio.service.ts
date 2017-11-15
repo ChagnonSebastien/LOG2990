3f8555403e5255ee54aeb7141c031357ec8f6de1
Audrey Labrie <audrey.labrie@polymtl.ca>
2017-11-15 14:06:39 -0500

    [feature] added attributs for each of the sounds

--- Cadriciel/client/src/app/racing-game/audio.service.ts
+++ Cadriciel/client/src/app/racing-game/audio.service.ts
@@ -3,10 +3,19 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AudioService {
     private countdown: HTMLAudioElement;
+    private race: HTMLAudioElement;
+    private stinger: HTMLAudioElement;
+    private themed: HTMLAudioElement;
 
     constructor() {
         this.countdown = new Audio('../../assets/sounds/countdown.mp3');
         this.countdown.load();
+        this.race = new Audio('../../assets/sounds/race.mp3');
+        this.race.load();
+        this.stinger = new Audio('../../assets/sounds/stinger.mp3');
+        this.stinger.load();
+        this.themed = new Audio('../../assets/sounds/themed.mp3');
+        this.themed.load();
     }
 
     public startCountdown() {
