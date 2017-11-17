f8034803dc9780ca58df0de2352c38788e9ce0b3
Audrey Labrie <audrey.labrie@polymtl.ca>
2017-11-16 20:24:51 -0500

    [feature] added observable and alert in race service to alert when race ends

--- Cadriciel/client/src/app/racing-game/race.service.ts
+++ Cadriciel/client/src/app/racing-game/race.service.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class RaceService {
+    private raceEndedSubject: Subject<any>;
+
+    constructor() {
+        this.raceEndedSubject = new Subject();
+    }
+
+    public raceEndedAlerts(): Observable<any> {
+        return this.raceEndedSubject.asObservable();
+    }
+
+    private endRace() {
+        this.raceEndedSubject.next();
+    }
 
 }
