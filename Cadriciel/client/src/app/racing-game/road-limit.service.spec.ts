ffe41901ff1c7cb7a0bad2d724bca506c0ade84b
Unknown <chagnon.s21@gmail.com>
2017-11-22 03:11:41 -0500

    RoadLimitServiceAdded, stopping the carts when leaving the track

---
+++ Cadriciel/client/src/app/racing-game/road-limit.service.spec.ts
@@ -0,0 +1,22 @@
+import { VehicleMoveEventService } from './vehicle-move-event.service';
+import { LineCalculationService } from './line-calculation.service';
+import { TestBed } from '@angular/core/testing';
+import { RoadLimitService } from './road-limit.service';
+
+let roadLimitService: RoadLimitService;
+
+describe('RoadLimitService', () => {
+
+      beforeEach(() => {
+        TestBed.resetTestingModule();
+        TestBed.configureTestingModule({
+            providers: [RoadLimitService, LineCalculationService, VehicleMoveEventService]
+        });
+        roadLimitService = TestBed.get(RoadLimitService);
+    });
+
+    it('should be created', () => {
+        expect(roadLimitService).toBeDefined();
+    });
+
+});
