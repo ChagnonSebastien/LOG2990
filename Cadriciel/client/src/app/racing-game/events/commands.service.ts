559f6b104395d7ccb4c80bc97584a6ff34415d69
Unknown <chagnon.s21@gmail.com>
2017-11-23 16:38:02 -0500

    Added a new enum PlayerCommand and the CommandService now sends these commands instead of a generic json object

--- Cadriciel/client/src/app/racing-game/events/commands.service.ts
+++ Cadriciel/client/src/app/racing-game/events/commands.service.ts
@@ -2,24 +2,68 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 
+export enum PlayerCommand {
+    MOVE_FORWARD,
+    ROTATE_LEFT,
+    ROTATE_RIGHT,
+    TOGGLE_NIGHT_MODE,
+    TOOGLE_CAMERA_VIEW,
+    ZOOM_IN,
+    ZOOM_OUT,
+    START_GAME
+}
+
 @Injectable()
 export class CommandsService {
-    private subjectEventKeyDown = new Subject();
-    private subjectEventKeyUp = new Subject();
+    private commandKeyDownEvent = new Subject<CommandEvent>();
+    private commandKeyUpEvent = new Subject<CommandEvent>();
 
-    public sendKeyDownEvent(event) {
-            this.subjectEventKeyDown.next(event);
+    public keyDown(event: any): void {
+        this.commandKeyDownEvent.next(new CommandEvent(this.getCommand(event.keyCode)));
     }
 
-    public getKeyDownEvent(): Observable<any> {
-        return this.subjectEventKeyDown.asObservable();
+    public keyUp(event: any): void {
+        this.commandKeyUpEvent.next(new CommandEvent(this.getCommand(event.keyCode)));
     }
 
-    public sentKeyUpEvent(event) {
-        this.subjectEventKeyUp.next(event);
+    public getCommandKeyDownObservable(): Observable<any> {
+        return this.commandKeyDownEvent.asObservable();
     }
 
-    public getKeyUpEvent(): Observable<any> {
-        return this.subjectEventKeyUp.asObservable();
+    public getCommandKeyUpObservable(): Observable<any> {
+        return this.commandKeyUpEvent.asObservable();
+    }
+
+    private getCommand(keyNumber: number): PlayerCommand {
+        switch (keyNumber) {
+            case 87:
+            return PlayerCommand.MOVE_FORWARD;
+            case 65:
+            return PlayerCommand.ROTATE_LEFT;
+            case 68:
+            return PlayerCommand.ROTATE_RIGHT;
+            case 78:
+            return PlayerCommand.TOGGLE_NIGHT_MODE;
+            case 67:
+            return PlayerCommand.TOOGLE_CAMERA_VIEW;
+            case 107:
+            return PlayerCommand.ZOOM_IN;
+            case 109:
+            return PlayerCommand.ZOOM_OUT;
+            case 32:
+            return PlayerCommand.START_GAME;
+            default:
+            return null;
+        }
+    }
+}
+
+export class CommandEvent {
+    constructor(
+        private command: PlayerCommand
+    ) {}
+
+    public getCommand(): PlayerCommand {
+        return this.command;
     }
 }
