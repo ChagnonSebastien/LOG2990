0cef6d2d5606f96f77390bd05b227891b94886ff
Audrey Labrie <audrey.labrie@polymtl.ca>
2017-11-24 12:28:34 -0500

    [refactor] removed pure methods from lapCounterService to TrackUtilities module

--- Cadriciel/client/src/app/racing-game/lap-counter.service.ts
+++ Cadriciel/client/src/app/racing-game/lap-counter.service.ts
@@ -1,9 +1,9 @@
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-
 import { VehicleService } from './vehicle.service';
 import { RenderService } from './render.service';
 import { Injectable } from '@angular/core';
 import * as THREE from 'three';
+import { TrackUtilities } from './track-utilities';
 
 const TRACK_RADIUS = 5;
 @Injectable()
@@ -12,68 +12,47 @@ export class LapCounterService {
     private passedCounter: Array<number>;
     private laps: BehaviorSubject<number>;
 
-
     constructor(
         private renderService: RenderService,
         private vehicleService: VehicleService
     ) {
         this.laps = new BehaviorSubject(0);
         this.lastIntersectionNumber = 0;
-        this.passedCounter = new Array<number>(this.numberOfIntersections()).fill(0);
+        this.passedCounter = new Array<number>(TrackUtilities.numberOfIntersections()).fill(0);
+        this.subscribeToRenderFrame();
+    }
+
+    private subscribeToRenderFrame(): void {
         this.renderService.frame.subscribe(() => {
             this.updatePassedCounter();
             this.updateLap();
         });
     }
 
-    public calculateDistanceFromIntersection(carPosition: THREE.Vector3, intersection: THREE.Vector2): number {
-        const distanceY = Math.abs(carPosition.y - intersection.y);
-        const distanceX = Math.abs(carPosition.x - intersection.x);
-        const distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
-
-        return distance;
-    }
-
-    public isAtIntersection(carPosition: THREE.Vector3, intersection: THREE.Vector2): boolean {
-        const distanceFromIntersection = this.calculateDistanceFromIntersection(carPosition, intersection);
-
-        return distanceFromIntersection <= TRACK_RADIUS;
-    }
-
-    private getIntersections(): Array<THREE.Vector2> {
-        return this.vehicleService.mainVehicle.getTrack().trackIntersections;
-    }
-
-    private numberOfIntersections(): number {
-        return this.getIntersections().length;
-    }
-
-    private updatePassedCounter() {
-        const position = this.getVehiclePosition();
-        const nextIntersectionNumber = (this.lastIntersectionNumber + 1) % this.numberOfIntersections();
-        const previousIntersectionNumber = (this.lastIntersectionNumber - 1) % this.numberOfIntersections();
-        const nextIntersection = this.getIntersections()[nextIntersectionNumber];
-        const previousIntersection = this.getIntersections()[previousIntersectionNumber];
-        if (this.isAtIntersection(position, nextIntersection)) {
+    private updatePassedCounter(): void {
+        const position = TrackUtilities.getVehiclePosition();
+        const nextIntersectionNumber = (this.lastIntersectionNumber + 1) % TrackUtilities.numberOfIntersections();
+        const previousIntersectionNumber = (this.lastIntersectionNumber - 1) % TrackUtilities.numberOfIntersections();
+        const nextIntersection = TrackUtilities.getIntersections()[nextIntersectionNumber];
+        const previousIntersection = TrackUtilities.getIntersections()[previousIntersectionNumber];
+        if (TrackUtilities.isAtIntersection(position, nextIntersection)) {
             this.passedCounter[nextIntersectionNumber]++;
             this.lastIntersectionNumber = nextIntersectionNumber;
-        } else if (this.isAtIntersection(position, previousIntersection)) {
+        } else if (TrackUtilities.isAtIntersection(position, previousIntersection)) {
             this.passedCounter[previousIntersectionNumber]--;
             this.lastIntersectionNumber = previousIntersectionNumber;
         }
     }
 
-    private getVehiclePosition(): THREE.Vector3 {
-        return this.vehicleService.mainVehicle.getVehicle().position;
-    }
-
     private passedFinishLine(): boolean {
-        const distanceToIntersectionZero = this.calculateDistanceFromIntersection(this.getVehiclePosition(), this.getIntersections()[0]);
-        const distanceToIntersectionOne = this.calculateDistanceFromIntersection(this.getVehiclePosition(), this.getIntersections()[1]);
+        const distanceToIntersectionZero = TrackUtilities.calculateDistanceFromIntersection(
+            TrackUtilities.getVehiclePosition(), TrackUtilities.getIntersections()[0]);
+        const distanceToIntersectionOne = TrackUtilities.calculateDistanceFromIntersection(
+            TrackUtilities.getVehiclePosition(), TrackUtilities.getIntersections()[1]);
         return distanceToIntersectionOne < distanceToIntersectionZero;
     }
 
-    private updateLap() {
+    private updateLap(): void {
         const minPassed = this.passedCounter.reduce((prev, next) => {
             return prev < next ? prev : next;
         });
