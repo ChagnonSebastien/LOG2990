3916285f9b5f10521cebffcf216b5c5180637bf7
jeanpaulcech <jeanpaulcech@yahoo.ca>
2017-11-24 19:08:23 -0500

    calculate the norm in the proper direction

--- Cadriciel/client/src/app/racing-game/collision-resolve.service.ts
+++ Cadriciel/client/src/app/racing-game/collision-resolve.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Vehicle } from './vehicle';
 import * as THREE from 'three';
 @Injectable()
 export class CollisionResolveService {
 
   constructor() { }
 
-  private resolveCollision(vehicleA: THREE.Mesh, vehicleB: THREE.Mesh, xCollisionPoint: number,
+  private resolveCollision(vehicleA: Vehicle, vehicleB: Vehicle, xCollisionPoint: number,
     zCollisionPoint: number, xCollisionPlanePoint, zCollisionPlanePoint) {
 
   }
@@ -13,15 +14,25 @@ export class CollisionResolveService {
   private calculateNormal(xCollisionPoint: number, zCollisionPoint: number, xCollisionPlanePoint, zCollisionPlanePoint): THREE.Vector3 {
     const dx = xCollisionPlanePoint - xCollisionPoint;
     const dz = zCollisionPlanePoint - zCollisionPoint;
-    const normal = new THREE.Vector3(dx, 0, dz);
+    const normal = new THREE.Vector3(-dz, 0, dx);
     return normal;
   }
 
-  private calculateDistanceVector(xVehiculePoint: number , zVehiculePoint: number, xCollisionPoint, zCollisionPoint) {
-    const dx = xCollisionPoint - xVehiculePoint;
-    const dz = zCollisionPoint - zVehiculePoint;
+  private calculateDistanceVector(xPoint1: number, zPoint1: number, xPoint2: number, zPoint2: number): THREE.Vector3 {
+    const dx = xPoint2 - xPoint1;
+    const dz = zPoint2 - zPoint1;
     const vector = new THREE.Vector3(dx, 0, dz);
     return vector;
+  }
+
+  private getCorrectNormalDirection(normal: THREE.Vector3, vehicleA: Vehicle, vehicleB: Vehicle): void {
+    const distanceVector = this.calculateDistanceVector(vehicleA.getVehicle().position.x,
+      vehicleA.getVehicle().position.z, vehicleB.getVehicle().position.x, vehicleB.getVehicle().position.z);
+
+    if ((distanceVector.x < 0 && normal.x > 0) || (distanceVector.x > 0 && normal.x < 0)
+      || (distanceVector.z < 0 && normal.z > 0) || (distanceVector.z > 0 && normal.z < 0)) {
+      normal.z = normal.z * -1;
+    }
   }
 
 }
