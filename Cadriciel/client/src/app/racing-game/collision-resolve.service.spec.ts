1810eefec39bd944ba66a8fa71084d1bf387df1b
jeanpaulcech <jeanpaulcech@yahoo.ca>
2017-11-24 22:28:57 -0500

    tests for the calculation of the normal

--- Cadriciel/client/src/app/racing-game/collision-resolve.service.spec.ts
+++ Cadriciel/client/src/app/racing-game/collision-resolve.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
-
 import { CollisionResolveService } from './collision-resolve.service';
-
+import * as THREE from 'three';
 describe('CollisionResolveService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -11,5 +10,26 @@ describe('CollisionResolveService', () => {
 
   it('should be created', inject([CollisionResolveService], (service: CollisionResolveService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should calculate the distance vector', inject([CollisionResolveService], (service: CollisionResolveService) => {
+    const distanceVector = service.calculateDistanceVector(2, 2, 4, 5);
+    const result = new THREE.Vector3(2, 0, 3);
+    expect(distanceVector).toEqual(result);
+  }));
+
+  it('should calculate the normal', inject([CollisionResolveService], (service: CollisionResolveService) => {
+    const normal = service.calculateNormal(5, 5, 9, 2);
+    const result = new THREE.Vector3(3, 0, 4);
+    expect(normal).toEqual(result);
+  }));
+
+  it('should calculate normal in the correct direction', inject([CollisionResolveService], (service: CollisionResolveService) => {
+    const normalExpected = new THREE.Vector3(-3 / 5, 0, -4 / 5);
+    const normal = new THREE.Vector3(3, 0, 4);
+    service.getCorrectNormalDirection(normal, 3, 2, 11, 3);
+    expect(normal.x).toBeCloseTo(normalExpected.x);
+    expect(normal.y).toBeCloseTo(normalExpected.y);
+    expect(normal.z).toBeCloseTo(normalExpected.z);
   }));
 });
--- Cadriciel/client/src/app/racing-game/collision-resolve.service.ts
+++ Cadriciel/client/src/app/racing-game/collision-resolve.service.ts
@@ -6,33 +6,34 @@ export class CollisionResolveService {
 
   constructor() { }
 
-  private resolveCollision(vehicleA: Vehicle, vehicleB: Vehicle, xCollisionPoint: number,
+  public resolveCollision(vehicleA: Vehicle, vehicleB: Vehicle, xCollisionPoint: number,
     zCollisionPoint: number, xCollisionPlanePoint, zCollisionPlanePoint) {
-
   }
 
-  private calculateNormal(xCollisionPoint: number, zCollisionPoint: number, xCollisionPlanePoint, zCollisionPlanePoint): THREE.Vector3 {
+  public calculateNormal(xCollisionPoint: number, zCollisionPoint: number, xCollisionPlanePoint, zCollisionPlanePoint): THREE.Vector3 {
     const dx = xCollisionPlanePoint - xCollisionPoint;
     const dz = zCollisionPlanePoint - zCollisionPoint;
     const normal = new THREE.Vector3(-dz, 0, dx);
     return normal;
   }
 
-  private calculateDistanceVector(xPoint1: number, zPoint1: number, xPoint2: number, zPoint2: number): THREE.Vector3 {
+  public calculateDistanceVector(xPoint1: number, zPoint1: number, xPoint2: number, zPoint2: number): THREE.Vector3 {
     const dx = xPoint2 - xPoint1;
     const dz = zPoint2 - zPoint1;
     const vector = new THREE.Vector3(dx, 0, dz);
     return vector;
   }
 
-  private getCorrectNormalDirection(normal: THREE.Vector3, vehicleA: Vehicle, vehicleB: Vehicle): void {
-    const distanceVector = this.calculateDistanceVector(vehicleA.getVehicle().position.x,
-      vehicleA.getVehicle().position.z, vehicleB.getVehicle().position.x, vehicleB.getVehicle().position.z);
+  public getCorrectNormalDirection(normal: THREE.Vector3, xVehicleA: number, zvehicleA: number,
+    xVehicleB: number, zvehicleB: number): void {
+    const distanceVector = this.calculateDistanceVector(xVehicleB, zvehicleB, xVehicleA, zvehicleA);
 
-    if ((distanceVector.x < 0 && normal.x > 0) || (distanceVector.x > 0 && normal.x < 0)
-      || (distanceVector.z < 0 && normal.z > 0) || (distanceVector.z > 0 && normal.z < 0)) {
+    if ((distanceVector.x < 0 && normal.x > 0) || (distanceVector.x > 0 && normal.x < 0)) {
+      normal.x = normal.x * -1;
+    }
+    if ((distanceVector.z < 0 && normal.z > 0) || (distanceVector.z > 0 && normal.z < 0)) {
       normal.z = normal.z * -1;
     }
+    normal.normalize();
   }
-
 }
