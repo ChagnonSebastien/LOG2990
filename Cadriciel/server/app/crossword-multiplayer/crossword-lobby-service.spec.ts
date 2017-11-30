2b64b75ca5d4f76e871758bb69da5e27e0768b54
Eric <sida.li@polymtl.ca>
2017-11-30 10:28:41 -0500

    Lobby service tested

--- Cadriciel/server/app/crossword-multiplayer/crossword-lobby-service.spec.ts
+++ Cadriciel/server/app/crossword-multiplayer/crossword-lobby-service.spec.ts
@@ -5,6 +5,7 @@ import { CrosswordLobbyService } from './crossword-lobby-service';
 import { CrosswordGamesManager } from './crossword-games-manager';
 
 import { TEST_HOST, TEST_PORT } from '../../testing/mock-socket-server';
+import { CrosswordGameInfo } from '../../../commun/crossword/crossword-game-info';
 
 let lobbyService: CrosswordLobbyService;
 let gamesManager: CrosswordGamesManager;
@@ -24,7 +25,7 @@ describe('#CrosswordLobbyService', () => {
         });
     });
 
-    afterEach(() => {
+    after(() => {
         if (socket.connected) {
             socket.disconnect();
         }
@@ -34,6 +35,48 @@ describe('#CrosswordLobbyService', () => {
         it('should return a singleton', () => {
             expect(CrosswordLobbyService.getInstance())
                 .to.equal(CrosswordLobbyService.getInstance());
+        });
+    });
+
+    describe('listenForLobbyRequests()', () => {
+        before(() => {
+            lobbyService.listenForLobbyRequests();
+        });
+
+        it('should listen for create game socket requests', () => {
+            expect(gamesManager.getAvailableGames.length).to.equal(0);
+            socket.emit('create game', {
+                difficulty: 'easy',
+                mode: 'classic',
+                username: 'fakeUsername'
+            });
+        });
+
+        it('should listen for get games socket requests', (done) => {
+            socket.on('sent all games', (games: CrosswordGameInfo) => {
+                done();
+            });
+            socket.emit('get games');
+        });
+
+        it('should listen for join game socket requests', () => {
+            socket.emit('join game', { gameId: 0 });
+        });
+
+        it('should listen for restart game socket requests', () => {
+            socket.emit('restart game', {
+                difficulty: 'easy',
+                mode: 'classic',
+                username: 'fakeUsername'
+            });
+        });
+
+        it('should listen for socket disconnections', () => {
+            socket.emit('disconnect');
+        });
+
+        it('should listen for leave game socket requests', () => {
+            socket.emit('leaveGame');
         });
     });
 });
