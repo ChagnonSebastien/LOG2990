fa9b04c73c60cd86d906224904c7125b842c3bc1
jeanpaulcech <jeanpaulcech@yahoo.ca>
2017-11-30 22:23:14 -0500

    added car engine sounds

--- Cadriciel/client/src/app/racing-game/audio.service.ts
+++ Cadriciel/client/src/app/racing-game/audio.service.ts
@@ -1,6 +1,7 @@
 import { RaceService } from './events/race.service';
 import { CollisionEventService } from './events/collision-event.service';
 import { CommandsService } from './events/commands.service';
+import { PlayerCommand } from './events/commands.service';
 import { Injectable } from '@angular/core';
 
 
@@ -11,13 +12,19 @@ export class AudioService {
     private stinger: HTMLAudioElement;
     private themed: HTMLAudioElement;
     private carCarCollision: HTMLAudioElement;
-    private engine: HTMLAudioElement;
+    private engineStart: HTMLAudioElement[] = [];
+    private idleEngine: HTMLAudioElement;
+    private accelerate: HTMLAudioElement;
+    private hitWall: HTMLAudioElement;
+    private hitPothole: HTMLAudioElement;
+    private acceleratorBonusStart: HTMLAudioElement;
+    private acceleratorBonusEnd: HTMLAudioElement;
 
     constructor(private raceService: RaceService, private collisionEventService: CollisionEventService,
         private commandsService: CommandsService) {
         this.listenForEndOfRace();
         this.listenForCarCarCollision();
-        this.listenForMoveForward();
+        this.listenForMoveVehicle();
         this.countdown = new Audio('../../assets/sounds/countdown.mp3');
         this.countdown.load();
         this.race = new Audio('../../assets/sounds/race.mp3');
@@ -28,61 +35,87 @@ export class AudioService {
         this.themed.load();
         this.carCarCollision = new Audio('../../assets/sounds/car_car_collision.mp3');
         this.carCarCollision.load();
-        this.engine = new Audio('../../assets/sounds/vrombissement.mp3');
-        this.engine.load();
+        for (let i = 0; i < 4; i++) {
+            this.engineStart[i] = new Audio('../../assets/sounds/carStart.mp3');
+            this.engineStart[i].load();
+        }
+        this.idleEngine = new Audio('../../assets/sounds/idleEngine.mp3');
+        this.idleEngine.load();
+        this.accelerate = new Audio('../../assets/sounds/carAcceleration.mp3');
     }
 
-    public startCountdown() {
+    public startCountdown(): void {
         this.countdown.play();
+        for (let i = 0; i < 4; i++) {
+            this.engineStart[i].play();
+        }
         this.countdown.addEventListener('ended', () => {
             this.startRace();
         });
     }
 
-    public startRace() {
+    public startRace(): void {
         this.race.loop = true;
+        this.idleEngine.loop = true;
+        this.idleEngine.play();
         this.race.play();
     }
 
-    public stopRace() {
+    public stopRace(): void {
         this.race.pause();
     }
 
-    public startCarCarCollision() {
+    private startCarCarCollision(): void {
+        this.engineStopAcccelerate();
         this.carCarCollision.play();
     }
 
-    public startEngine() {
-        this.engine.play();
+    private engineAccelerate(): void {
+        this.accelerate.loop = true;
+        this.accelerate.play();
     }
 
-    private listenForEndOfRace() {
+    private engineStopAcccelerate(): void {
+        console.log('stop');
+        this.accelerate.pause();
+        this.accelerate.currentTime = 0;
+    }
+
+    private listenForEndOfRace(): void {
         this.raceService.raceEndedAlerts().subscribe(() => {
             this.stopRace();
             this.startStinger();
         });
     }
 
-    private listenForCarCarCollision() {
+    private listenForCarCarCollision(): void {
         this.collisionEventService.getCollisionObservable().subscribe(() => {
             this.startCarCarCollision();
         });
     }
 
-    private listenForMoveForward() {
-        this.commandsService.getCommandKeyUpObservable().subscribe(() => {
-            this.startEngine();
+    private listenForMoveVehicle(): void {
+        this.commandsService.getCommandKeyDownObservable().subscribe((event) => {
+            if (event.getCommand() === PlayerCommand.MOVE_FORWARD) {
+                this.engineAccelerate();
+            }
         });
     }
-
-    private startStinger() {
+    private listenForStopMoveVehicle(): void {
+        this.commandsService.getCommandKeyUpObservable().subscribe((event) => {
+            if (event.getCommand() === PlayerCommand.MOVE_FORWARD) {
+                this.engineStopAcccelerate();
+            }
+        });
+    }
+    private startStinger(): void {
         this.stinger.play();
         this.stinger.addEventListener('ended', () => {
             this.startThemed();
         });
     }
 
-    private startThemed() {
+    private startThemed(): void {
         this.themed.loop = true;
         this.themed.play();
     }
