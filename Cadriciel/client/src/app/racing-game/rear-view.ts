537a7d3a177819e53eafcf17c48e882d1ede6ae4
Julio <julio-herve.dandjinou@polymtl.ca>
2017-12-01 14:56:34 -0500

    Magic numbers

--- Cadriciel/client/src/app/racing-game/rear-view.ts
+++ Cadriciel/client/src/app/racing-game/rear-view.ts
@@ -1,7 +1,12 @@
-import { Scene, PerspectiveCamera, Object3D } from 'three';
+import { Scene, PerspectiveCamera, Object3D, Vector3 } from 'three';
+
+const WEIGHT = 300;
+const HEIGHT = 150;
+const ORIGIN_X = 400;
+const ORIGIN_Y = 650;
+const FAR = 3000;
 
 export class RearView {
-
     private scene: Scene;
     private rearCamera: PerspectiveCamera;
     private renderer: THREE.WebGLRenderer;
@@ -15,14 +20,14 @@ export class RearView {
     public addRearView(vehicle: any) {
         const  target = new Object3D;
         vehicle.add(target);
-        target.position.set(0, 0, 3000);
+        target.position.set(0, 0, FAR);
         vehicle.add(this.rearCamera);
 
         this.rearCamera.position.set(0, 1, 2);
         this.rearCamera.lookAt(target.position);
 
-        this.renderer.setViewport(400, 650, 300, 150);
-        this.renderer.setScissor(400, 650, 300, 150);
+        this.renderer.setViewport(ORIGIN_X, ORIGIN_Y, WEIGHT, HEIGHT);
+        this.renderer.setScissor(ORIGIN_X, ORIGIN_Y, WEIGHT, HEIGHT);
         this.renderer.setScissorTest(true);
         this.renderer.render(this.scene, this.rearCamera);
 
