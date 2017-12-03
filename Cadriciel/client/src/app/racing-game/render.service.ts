ebbf8451e91aa71fbfcee8b021cb9e621152f125
Audrey Labrie <audrey.labrie@polymtl.ca>
2017-12-03 07:59:26 -0500

    [feature] two viewports in scene for hud

--- Cadriciel/client/src/app/racing-game/render.service.ts
+++ Cadriciel/client/src/app/racing-game/render.service.ts
@@ -44,11 +44,11 @@ export class RenderService {
         this.frameEventService.sendFrameEvent(new FrameEvent());
 
         this.renderer.clear();
-        this.renderer.setViewport(0, 0, 1866, 1186);
+        this.renderer.setViewport(0, 0, 1866, 1186 / 2);
         this.renderer.render(this.sceneService.scene, this.cameraService.getCamera());
 
         this.renderer.clearDepth();
-        this.renderer.setViewport(10, 10, 500, 500);
+        this.renderer.setViewport(0, 1186 / 2, 1866, 1186 / 2);
         this.renderer.render(this.raceService.sceneHud, this.raceService.cameraHud);
         this.stats.update();
     }
