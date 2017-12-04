0ada5ba1534a794d1f875ac8b6744cf6f8a3783a
Eric <sida.li@polymtl.ca>
2017-12-04 14:47:33 -0500

    (refactor) crosswordSchema

--- Cadriciel/server/app/routes/crosswordSchema.ts
+++ Cadriciel/server/app/routes/crosswordSchema.ts
@@ -1,26 +1,20 @@
 import * as mongoose from 'mongoose';
+import { DATABASE_URL, CROSSWORD_COLLECTION } from '../config';
 
-/* mongoose.connect(
-    'mongodb://LOG2990-03:yJ96PW80@parapluie.info.polymtl.ca:27017/LOG2990-03-db',
-    { useMongoClient: true }
-); */
 mongoose.connect(
-    'mongodb://admin:walleandtomato@ds123084.mlab.com:23084/skafy',
+    DATABASE_URL,
     { useMongoClient: true }
 );
 
-// Get the default connection
 const db = mongoose.connection;
-
-// Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const crosswordSchema = new mongoose.Schema({
 
     crossword:
-    {
-        type: [[String]]
-    },
+        {
+            type: [[String]]
+        },
     difficulty: {
         type: String
     },
@@ -28,9 +22,9 @@ const crosswordSchema = new mongoose.Schema({
         type: [String]
     },
     wordsWithIndex: {
-        type: [{i: Number, j: Number, word: String, horizontal: Boolean}]
+        type: [{ i: Number, j: Number, word: String, horizontal: Boolean }]
     }
 });
 
 
-module.exports = mongoose.model('crosswords', crosswordSchema);
+module.exports = mongoose.model(CROSSWORD_COLLECTION, crosswordSchema);
