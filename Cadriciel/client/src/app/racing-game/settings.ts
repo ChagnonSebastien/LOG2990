b754bd747a900dd0a19077d711cf93415a9a445a
jeanpaulcech <jeanpaulcech@yahoo.ca>
2017-12-04 23:06:42 -0500

    removed hard coded path for the night skybox by using setting constants instead

--- Cadriciel/client/src/app/racing-game/settings.ts
+++ Cadriciel/client/src/app/racing-game/settings.ts
@@ -22,6 +22,13 @@ export module Settings {
     export const SOUND_ACCELERATOR_BONUS_START = 'sounds/acceleratorBonusStart.mp3';
     export const SOUND_ACCELERATOR_BONUS_END = 'sounds/acceleratorBonusEnd.mp3';
     export const SOUND_HIT_WALL = 'sounds/hitWall.mp3';
+    export const SKYBOX_IMAGE_FOLDER = 'images/skybox/';
+    export const SKYBOX_IMAGE_NIGHT_UP = 'bluefreeze_up.png';
+    export const SKYBOX_IMAGE_NIGHT_RT = 'bluefreeze_rt.png';
+    export const SKYBOX_IMAGE_NIGHT_LF = 'bluefreeze_lf.png';
+    export const SKYBOX_IMAGE_NIGHT_DN = 'bluefreeze_dn.png';
+    export const SKYBOX_IMAGE_NIGHT_FT = 'bluefreeze_ft.png';
+    export const SKYBOX_IMAGE_NIGHT_BK = 'bluefreeze_bk.png';
 
     export const MUSIC_ENDED_EVENT = 'ended';
 
